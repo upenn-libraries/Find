3f3d22d3d4983d11ad4ab3f7611c4ab56b700b94
Amrey Mathurin <mamrey@upenn.edu>
2023-11-10 08:53:57 -0500

    Document advance search configuration

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -176,13 +176,15 @@ class CatalogController < ApplicationController
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
+    # Add search fields to blacklight's built-in advanced search form.
+    # Advanced search relies on solr's json query dsl. In order to make a valid json query, we have to include our
+    # search parameters in a clause_params hash.
     QUERY_FIELDS.each do |query_field|
       next if query_field.in? %i[id isxn_search]
 
       label = I18n.t("advanced.#{query_field.to_s.split('_').first}")
 
       config.add_search_field(query_field, label: label) do |field|
-        # solr_parameters hash are sent to Solr as ordinary url query params.
         field.include_in_advanced_search = true
         field.include_in_simple_select = false
         field.clause_params = { edismax: { qf: query_field, pf: query_field } }
