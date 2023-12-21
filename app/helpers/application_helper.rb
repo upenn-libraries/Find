6c2da16d5123dd1ab79a59d98624bccf12e147e2
Mike Kanning <mkanning@upenn.edu>
2023-12-21 12:00:26 -0500

    update pennmarc, update field names in config per pennmarc changes. add helper methods for displaying structured link hashes and subject links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,40 @@
 
 # Base helper for application views
 module ApplicationHelper
+  # Render Subject heading values as facet field links
+  # @param [Hash] options
+  # @return [ActiveSupport::SafeBuffer]
+  def render_subject_facet_links(options)
+    safe_join_links(
+      options[:value].map { |value|
+        link_to value, search_catalog_path({ 'f[subject_facet]': value })
+      }.sort
+    )
+  end
+
   # Render JSON array field of link hashes into <br> delimited HTML
   # @param [Hash] options
-  # @return [String]
-  def render_as_links(options)
-    links = JSON.parse(options[:value].first)
-    link_html = links.map { |link_info|
-      link_to link_info['link_text'], link_info['link_url']
-    }.join('<br/>')
-    sanitize(link_html, tags: %w[a br], attributes: ['href'])
+  # @return [ActiveSupport::SafeBuffer]
+  def render_links_from_hash(options)
+    # If the field is stored, it will be a JSON encoded string
+    link_data = if options[:value].first.is_a? String
+                  JSON.parse(options[:value].first, symbolize_names: true)
+                else
+                  options[:value]
+                end
+    safe_join_links(
+      link_data.map do |link_info|
+        link_to link_info[:link_text], link_info[:link_url]
+      end
+    )
+  end
+
+  # @param [Array<String>] links
+  # @return [ActiveSupport::SafeBuffer]
+  def safe_join_links(links)
+    sanitize(
+      links.join('<br/>'),
+      tags: %w[a br], attributes: ['href']
+    )
   end
 end
