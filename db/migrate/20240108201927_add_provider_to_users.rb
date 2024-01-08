13357054d7efa0d10c03e687e356b5a98e204f84
Patrick Perkins <lilblizzard97@gmail.com>
2024-01-08 17:24:23 -0500

    user schema changes

---
+++ db/migrate/20240108201927_add_provider_to_users.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AddProviderToUsers < ActiveRecord::Migration[7.0]
+  def change
+    change_table :users do |t|
+      # omniauthable
+      t.string :provider
+      t.string :uid
+
+      t.index %i[uid provider], unique: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_170919) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_08_201927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,8 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_170919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "guest", default: false, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
