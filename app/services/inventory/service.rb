8e98adff9f766424d46c910b2151e841b58f054b
Amrey Mathurin <mamrey@upenn.edu>
2024-01-17 13:10:11 -0500

    Define find_many method

--- app/services/inventory/service.rb
+++ app/services/inventory/service.rb
@@ -3,6 +3,8 @@
 module Inventory
   # Retrieves inventory from Alma real time availability api
   class Service
+    MAX_BIBS_GET = 100 # 100 is Alma API max
+    class Error < StandardError; end
     class << self
 
       # Retrieve real time availability of single inventory resource from Alma
@@ -18,12 +20,16 @@ module Inventory
       # @param [Array<String>] mms_ids
       # @param [Integer] brief_count
       # @return [Array<Hash>]
-      def find(mms_id, _brief_count = 3)
-        availability_data = Alma::Bib.get_availability([mms_id])
-        holdings(mms_id, holding_data(mms_id, availability_data)).map(&:to_h)
-      end
+      def find_many(mms_ids, brief_count = 3)
+        raise Error, "Too many MMS IDs provided, exceeds max allowed of #{MAX_BIBS_GET}." if mms_ids.size > MAX_BIBS_GET
 
-      def find_many; end
+        availability_data = Alma::Bib.get_availability(mms_ids)
+
+        mms_ids.map do |mms_id|
+          inventory_data  = inventory_data(mms_id, availability_data)
+          { mms_id.to_sym => { inventory: inventory(mms_id, inventory_data).map(&:to_h).first(brief_count) } }
+        end
+      end
 
       private
 
--- spec/services/inventory/service_spec.rb
+++ spec/services/inventory/service_spec.rb
@@ -1,37 +1,85 @@
 # frozen_string_literal: true
 
 describe Inventory::Service do
-  let(:mms_id) { '9979338417503681' }
+  let(:sample_electronic_bib_response) do
+    { '9979338417503681' =>
+        { holdings: [{ 'portfolio_pid' => '53478314060003681',
+                       'collection_id' => '61468362070003681',
+                       'activation_status' => 'Available',
+                       'collection' => 'Factiva',
+                       'coverage_statement' => 'Available from 01/01/1997 until 12/31/2021.',
+                       'interface_name' => 'Factiva',
+                       'inventory_type' => 'electronic' },
+                     { 'portfolio_pid' => '53693946490003681',
+                       'collection_id' => '61692149360003681',
+                       'activation_status' => 'Available',
+                       'collection' => 'Education Magazine Archive',
+                       'coverage_statement' => 'Available from 01/02/1919 until 12/31/1971.',
+                       'interface_name' => 'ProQuest',
+                       'inventory_type' => 'electronic' },
+                     { 'portfolio_pid' => '53526992570003681',
+                       'collection_id' => '61468377400003681',
+                       'activation_status' => 'Available',
+                       'collection' => 'PressReader',
+                       'coverage_statement' => 'Available from 07/28/2017 until 12/24/2021.',
+                       'interface_name' => 'PressReader',
+                       'inventory_type' => 'electronic' }] } }
+  end
+
+  let(:sample_physical_bib_response) do
+    { '9977048531103681' =>
+        { holdings: [{ 'holding_id' => '22810131440003681',
+                       'institution' => '01UPENN_INST',
+                       'library_code' => 'VanPeltLib',
+                       'location' => 'Stacks',
+                       'call_number' => 'HQ801 .D43 1997',
+                       'availability' => 'available',
+                       'total_items' => '1',
+                       'non_available_items' => '0',
+                       'location_code' => 'vanp',
+                       'call_number_type' => '0',
+                       'priority' => '1',
+                       'library' => 'Van Pelt Library',
+                       'inventory_type' => 'physical' }] } }
+  end
 
   describe '.find' do
-    let(:bib_response) do
-      { '9979338417503681' =>
-          { holdings: [{ 'holding_id' => '22810131440003681',
-                         'institution' => '01UPENN_INST',
-                         'library_code' => 'VanPeltLib',
-                         'location' => 'Stacks',
-                         'call_number' => 'HQ801 .D43 1997',
-                         'availability' => 'available',
-                         'total_items' => '1',
-                         'non_available_items' => '0',
-                         'location_code' => 'vanp',
-                         'call_number_type' => '0',
-                         'priority' => '1',
-                         'library' => 'Van Pelt Library',
-                         'inventory_type' => 'physical' }] } }
-    end
-    let(:holdings) { described_class.find('9979338417503681') }
+    let(:inventory) { described_class.find('9977048531103681') }
 
     before do
       availability_double = instance_double(Alma::AvailabilityResponse)
       allow(Alma::Bib).to receive(:get_availability).and_return(availability_double)
-      allow(availability_double).to receive(:availability).and_return(bib_response)
+      allow(availability_double).to receive(:availability).and_return(sample_physical_bib_response)
     end
 
-    it 'returns data' do
-      expect(holdings).to eq([{ count: '1', description: 'HQ801 .D43 1997', format: '',
-                                href: '/catalog/9979338417503681#22810131440003681', id: '22810131440003681',
-                                location: 'Van Pelt Library', policy: '', status: 'available', type: 'physical' }])
+    it 'returns expected inventory data' do
+      expect(inventory).to eq([{ count: '1', description: 'HQ801 .D43 1997', format: '',
+                                 href: '/catalog/9977048531103681#22810131440003681', id: '22810131440003681',
+                                 location: 'Van Pelt Library', policy: '', status: 'available', type: 'physical' }])
+    end
+  end
+
+  describe '.find_many' do
+    let(:inventory) { described_class.find_many(%w[9979338417503681 9977048531103681], 1) }
+    let(:response) { sample_physical_bib_response.merge(sample_electronic_bib_response) }
+
+    before do
+      availability_double = instance_double(Alma::AvailabilityResponse)
+      allow(Alma::Bib).to receive(:get_availability).and_return(availability_double)
+      allow(availability_double).to receive(:availability).and_return(response)
+    end
+
+    it 'returns expected inventory data' do
+      expect(inventory.size).to eq 2
+    end
+
+    it 'has the inventory data for each mms_id' do
+      expect(inventory.last).to(
+        eq({:'9977048531103681' => {inventory: [{ count: '1', description: 'HQ801 .D43 1997', id: '22810131440003681',
+                                                  location: 'Van Pelt Library', policy: '', status: 'available',
+                                                  type: 'physical', href: '/catalog/9977048531103681#22810131440003681',
+                                                  format: '' }]}})
+      )
     end
   end
 end
