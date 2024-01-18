a49d85f8784dbb384952814a5bfa040b92334ee9
Amrey Mathurin <mamrey@upenn.edu>
2024-01-18 15:22:19 -0500

    Build electronic inventory URL

--- app/services/inventory/electronic.rb
+++ app/services/inventory/electronic.rb
@@ -3,35 +3,43 @@
 module Inventory
   # Electronic holding class
   class Electronic < Base
-    # Define href as URI
-    # BASE_URL = URI.new
-    # @return [String]
+    # Base host, path, and params to electronic resource (portfolio)
+    HOST = 'upenn.alma.exlibrisgroup.com'
+    PATH = '/view/uresolver/01UPENN_INST/openurl'
+    PARAMS = { Force_direct: true,
+               portfolio_pid: nil,
+               rfr_id: 'info:sid/primo.exlibrisgroup.com',
+               'u.ignore_date_coverage': true }.freeze
+
+    # @return [String, nil]
     def status
       raw_api_data['activation_status']
     end
 
-    # @return [String]
+    # @return [String, nil]
     def policy; end
 
-    # @return [String]
+    # @return [String, nil]
     def description
       raw_api_data['collection']
     end
 
-    # @return [String, NilClass]
+    # @return [String, nil]
     def format; end
 
-    # @return [String]
+    # @return [String, nil]
     def id
       raw_api_data['portfolio_pid']
     end
 
-    # @return [String]
+    # @return [String, nil]
     def href
       return nil if id.blank?
 
-      'https://upenn.alma.exlibrisgroup.com/view/uresolver/01UPENN_INST/openurl?Force_direct=true&portfolio_pid=' \
-        "#{id}&rfr_id=info%3Asid%2Fprimo.exlibrisgroup.com&u.ignore_date_coverage=true"
+      params = { **PARAMS, portfolio_pid: id }
+      query = URI.encode_www_form(params)
+
+      URI::HTTPS.build(host: HOST, path: PATH, query: query).to_s
     end
   end
 end
