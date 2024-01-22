7e007afa725abb30b41445cb56cc88649e05c0a8
Patrick Perkins <lilblizzard97@gmail.com>
2024-01-22 15:33:08 -0500

    check for alma account before creating user account and fail with exception if alma user is not found

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,16 +3,28 @@
 module Users
   # custom Omniauth callbacks
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    skip_before_action :verify_authenticity_token, only: %i[developer saml failure]
+    skip_before_action :verify_authenticity_token, only: %i[developer failure]
 
     def developer
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      handle_user(user: @user, kind: 'developer')
+      auth_request = request.env['omniauth.auth']
+      if registered_in_alma?(auth_request['uid'])
+        @user = User.from_omniauth(auth_request)
+        handle_user(user: @user, kind: 'developer')
+      else
+        redirect_to login_path
+        set_flash_message(:alert, :alma_failure)
+      end
     end
 
     def saml
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      handle_user(user: @user, kind: 'saml')
+      auth_request = request.env['omniauth.auth']
+      if registered_in_alma?(auth_request['uid'])
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        handle_user(user: @user, kind: 'saml')
+      else
+        redirect_to login_path
+        set_flash_message(:alert, :alma_failure)
+      end
     end
 
     def failure
@@ -35,6 +47,13 @@ module Users
         set_flash_message(:notice, :failure, kind: kind, reason: user.errors.to_a.join(', ')) if is_navigational_format?
         redirect_to login_path
       end
+    end
+
+    def registered_in_alma?(uid)
+      user = Alma::User.find(uid)
+      user.instance_of?(Alma::User)
+    rescue Alma::User::ResponseError
+      false
     end
   end
 end
