5d0822be85f29762162a47a560d6c13e76f8b37a
Patrick Perkins <lilblizzard97@gmail.com>
2024-01-23 16:32:46 -0500

    oauth stubbing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -59,4 +59,10 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:developer] = {
+    'provider' => 'developer',
+    'uid' => 'aalten'
+  }
 end
