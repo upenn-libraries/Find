548b115ae9482845ca54ed58997e3b7ff62cd4c8
Jon Earley <earleyj@upenn.edu>
2024-02-02 10:27:00 -0500

    Add description and new line at end

--- app/components/find/search_button_component.rb
+++ app/components/find/search_button_component.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module Find
+  # For rendering "Find it" rather than blacklight's icon with a hidden label
+  # A label comes with more affordances such as being able to select the control
+  # with voice when an icon could be difficult to know how to target.
   class SearchButtonComponent < Blacklight::Component
     def initialize(text:, id:)
       @text = text
