aab0ebb1ebac1f776e324272de17f4bceba17f3b
Mike Kanning <mkanning@upenn.edu>
2024-02-19 15:52:52 -0500

    remove field rendering helpers - we will do this with componentry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,4 @@
 # frozen_string_literal: true
 
 # Base helper for application views
-module ApplicationHelper
-  # Render Subject heading values as facet field links
-  # @param [Hash] options
-  # @return [ActiveSupport::SafeBuffer]
-  def render_subject_facet_links(options)
-    safe_join_links(
-      options[:value].map { |value|
-        link_to value, search_catalog_path({ 'f[subject_facet]': value })
-      }.sort
-    )
-  end
-
-  # Render JSON array field of link hashes into <br> delimited HTML
-  # @param [Hash] options
-  # @return [ActiveSupport::SafeBuffer]
-  def render_links_from_hash(options)
-    # If the field is stored, it will be a JSON encoded string
-    link_data = if options[:value].first.is_a? String
-                  JSON.parse(options[:value].first, symbolize_names: true)
-                else
-                  options[:value]
-                end
-    safe_join_links(
-      link_data.map do |link_info|
-        link_to link_info[:link_text], link_info[:link_url]
-      end
-    )
-  end
-
-  # @param [Array<String>] links
-  # @return [ActiveSupport::SafeBuffer]
-  def safe_join_links(links)
-    sanitize(
-      links.join('<br/>'),
-      tags: %w[a br], attributes: ['href']
-    )
-  end
-end
+module ApplicationHelper; end
