186bbea1b4dfc3d5ecac58315a029e3db876329c
Patrick Perkins <lilblizzard97@gmail.com>
2024-02-21 17:14:36 -0500

    use join processor to render links - better than component? tbd...

--- app/processors/find/join_processor.rb
+++ app/processors/find/join_processor.rb
@@ -12,11 +12,24 @@ module Find
 
       joined = content_tag :ul, id: "#{config.key}-list", class: 'list-unstyled' do
         values.each do |value|
-          list_item = content_tag(:li, value)
+          list_item = content_tag(:li, format_value(value))
           concat list_item
         end
       end
+
       next_step(joined)
+    end
+
+    private
+
+    def format_value(value)
+      return value unless link_hash?(value)
+
+      content_tag(:a, value[:link_text], href: value[:link_url])
+    end
+
+    def link_hash?(value)
+      value.instance_of?(Hash) && value.key?(:link_text) && value.key?(:link_url)
     end
   end
 end
