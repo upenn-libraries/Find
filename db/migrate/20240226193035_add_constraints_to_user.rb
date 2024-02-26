5030d6fcfb7041b9c73e767873b9566bede6fc8a
Patrick Perkins <lilblizzard97@gmail.com>
2024-02-26 17:27:38 -0500

    appease cop

--- db/migrate/20240226193035_add_constraints_to_user.rb
+++ db/migrate/20240226193035_add_constraints_to_user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Prevent providers and uid's from storing null values
 class AddConstraintsToUser < ActiveRecord::Migration[7.0]
   def change
     change_column_null :users, :provider, false
