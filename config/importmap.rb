cd73dea4652f7510b6ede550f5ce96876e3f6bf7
Carla Galarza <cgalarza@upenn.edu>
2024-02-26 10:42:29 -0500

    Fixing rubocop offenses

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,8 +8,8 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin '@github/auto-complete-element', to: 'https://cdn.skypack.dev/@github/auto-complete-element'
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin '@popperjs/core', to: 'popper.js', preload: true
 
 # TODO: fix getAssetPath issue with components
 # pin '@penn-libraries/web', to: "https://cdn.jsdelivr.net/npm/@penn-libraries/web@#{Settings.pennlibs_web_version}/loader/index.js"
