7787a2fea7f994e311ce6e2f472dd6292522b73f
Carla Galarza <cgalarza@upenn.edu>
2024-02-26 10:22:28 -0500

    Using bootstrap javascript files provided by gem instead of importing from CDN. This ensure we are using the same bootstrap version for js and css files.

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,8 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin '@github/auto-complete-element', to: 'https://cdn.skypack.dev/@github/auto-complete-element'
-pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/umd/popper.min.js'
-pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.js'
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
 # TODO: fix getAssetPath issue with components
 # pin '@penn-libraries/web', to: "https://cdn.jsdelivr.net/npm/@penn-libraries/web@#{Settings.pennlibs_web_version}/loader/index.js"
