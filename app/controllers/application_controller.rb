e4558a8e563b21062e54a42443a3955c8b8d1645
Patrick Perkins <lilblizzard97@gmail.com>
2024-02-26 17:12:47 -0500

    override guest user params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   layout :determine_layout if respond_to? :layout
+
+  def guest_user_params
+    { provider: 'guest', uid: 'guest' }
+  end
 end
