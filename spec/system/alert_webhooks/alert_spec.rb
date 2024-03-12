9c2ec2f4582fab7a673c07506f91671895025c60
Patrick Perkins <lilblizzard97@gmail.com>
2024-03-12 10:33:44 -0400

    refactor alert system spec

--- spec/system/alert_webhooks/alert_spec.rb
+++ spec/system/alert_webhooks/alert_spec.rb
@@ -7,15 +7,14 @@ describe 'alert display' do
 
   before do
     allow(Rails.application.credentials).to receive(:alert_webhooks_token).and_return('1234')
+    scopes.each { |scope| create(:alert, scope: scope) }
+    post webhooks_alerts_path, params: json_fixture(fixture, :alert_webhooks), headers: { 'Token': '1234' }
+    visit root_path
   end
 
   context 'with a general alert' do
-    before do
-      create(:alert, scope: 'alert', text: '<p>Original General Alert</p>')
-      post webhooks_alerts_path, params: json_fixture('general_updated', :alert_webhooks),
-                                 headers: { 'Token': '1234' }
-      visit root_path
-    end
+    let(:scopes) { %w[alert] }
+    let(:fixture) { 'general_updated' }
 
     it 'displays updated general alert' do
       within('div.webhook-alerts') do
@@ -25,12 +24,8 @@ describe 'alert display' do
   end
 
   context 'with a find only alert' do
-    before do
-      create(:alert, scope: 'find_only_alert', text: '<p>Original Find Alert</p>')
-      post webhooks_alerts_path, params: json_fixture('find_only_updated', :alert_webhooks),
-                                 headers: { 'Token': '1234' }
-      visit root_path
-    end
+    let(:scopes) { %w[find_only_alert] }
+    let(:fixture) { 'find_only_updated' }
 
     it 'displays updated find only alert' do
       within('div.webhook-alerts') do
@@ -40,13 +35,8 @@ describe 'alert display' do
   end
 
   context 'with both alerts' do
-    before do
-      create(:alert, scope: 'alert')
-      create(:alert, scope: 'find_only_alert')
-      post webhooks_alerts_path, params: json_fixture('both_updated', :alert_webhooks),
-                                 headers: { 'Token': '1234' }
-      visit root_path
-    end
+    let(:scopes) { %w[alert find_only_alert] }
+    let(:fixture) { 'both_updated' }
 
     it 'displays both alerts' do
       within('div.webhook-alerts') do
