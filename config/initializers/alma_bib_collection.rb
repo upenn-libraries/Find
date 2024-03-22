832ad5bb3934be16bdc95c7e80580152ee642fcf
Mike Kanning <mkanning@upenn.edu>
2024-03-22 15:06:48 -0400

    Add justification for adding to Alma::Bib class in an initializer :/

--- config/initializers/alma_bib_collection.rb
+++ config/initializers/alma_bib_collection.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
+# The Alma API gem does not appear to expose the "Get E-Collection for a Bib" API endpoint. We need this in order to
+# grab any Inventory that is represented in Alma as an E-Collection without an associated portfolio.
+#
+# API Documentation: https://developers.exlibrisgroup.com/alma/apis/docs/bibs/R0VUIC9hbG1hd3MvdjEvYmlicy97bW1zX2lkfS9lLWNvbGxlY3Rpb25z/
+#
+# This initializer adds a class method get_ecollections to the Alma::Bib class used as the means for getting a top-level
+# view of any e-collections associated with a Bib.
+#
+# I wrote this in such a way that it could be offered as an MR to the Alma gem, eventually.
+#
+# rubocop:disable Style
 module AlmaGetBibCollections
+  # @param id [String]
+  # @return [Array]
   def get_ecollections(id, args = {})
     response = HTTParty.get(
       "#{self.bibs_base_path}/#{id}/e-collections",
@@ -16,3 +31,4 @@ module AlmaGetBibCollections
 end
 
 Alma::Bib.extend(AlmaGetBibCollections)
+# rubocop:enable Style
