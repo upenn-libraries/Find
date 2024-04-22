6e2942b208b33b2a574ba9d99deb0401328202b6
Amrey Mathurin <mamrey@upenn.edu>
2024-04-22 11:22:21 -0400

    Allow us to pass in hash when stubbing alma user group to retrieve both 'value' and 'desc' fields

--- spec/system/account/settings/show_spec.rb
+++ spec/system/account/settings/show_spec.rb
@@ -4,15 +4,13 @@ require 'system_helper'
 
 describe 'Account Settings show page' do
   let(:user) { build(:user) }
+  let(:alma_user_group) { { 'desc' => 'undergraduate' } }
+
+  include_context 'with mock alma_record on user having alma_user_group user group'
 
   context 'with a student user' do
     before do
       sign_in user
-
-      # Stub Alma User
-      alma_user = instance_double(Alma::User)
-      allow(user).to receive(:alma_record).and_return(alma_user)
-      allow(alma_user).to receive(:method_missing).with(:user_group).and_return({ 'desc' => 'undergraduate' })
 
       # Stub Illiad User
       illiad_user = build(:illiad_user, Address2: '123 private road / Philadelphia PA', Zip: '19104')
