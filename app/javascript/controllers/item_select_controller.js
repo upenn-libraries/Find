8a71af9f1bbc7102f62ea0afbc3f36e87c45b2d7
body-clock <lilblizzard97@gmail.com>
2024-04-24 21:51:44 -0400

    reformat stimulus controllers with prettier

--- app/javascript/controllers/item_select_controller.js
+++ app/javascript/controllers/item_select_controller.js
@@ -1,20 +1,25 @@
-import {Controller} from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = [
-        'select', 'mmsIdField', 'holdingIdField', 'commentsArea',
-        'optionsFrame', 'optionsLoadingTemplate'
-    ]
+  static targets = [
+    "select",
+    "mmsIdField",
+    "holdingIdField",
+    "commentsArea",
+    "optionsFrame",
+    "optionsLoadingTemplate",
+  ];
 
-    selectChanged(event) {
-        const mmsIdValue = this.mmsIdFieldTarget.value
-        const holdingValue = this.holdingIdFieldTarget.value
-        const itemValue = event.target.value
-        const url = `/account/requests/options?mms_id=${mmsIdValue}&holding_id=${holdingValue}&item_pid=${itemValue}`
+  selectChanged(event) {
+    const mmsIdValue = this.mmsIdFieldTarget.value;
+    const holdingValue = this.holdingIdFieldTarget.value;
+    const itemValue = event.target.value;
+    const url = `/account/requests/options?mms_id=${mmsIdValue}&holding_id=${holdingValue}&item_pid=${itemValue}`;
 
-        if (itemValue.length > 0) {
-            this.optionsFrameTarget.src = url
-            this.optionsFrameTarget.innerHTML = this.optionsLoadingTemplateTarget.innerHTML
-        }
+    if (itemValue.length > 0) {
+      this.optionsFrameTarget.src = url;
+      this.optionsFrameTarget.innerHTML =
+        this.optionsLoadingTemplateTarget.innerHTML;
     }
+  }
 }
