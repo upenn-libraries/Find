56eba8e00f5dcb6b8fc537f3eff3d40e9b6d9965
Patrick Perkins <lilblizzard97@gmail.com>
2024-04-26 12:43:38 -0400

    move inline faraday requests to gem extensions for easier stubbing

--- app/services/items/service.rb
+++ app/services/items/service.rb
@@ -4,30 +4,16 @@
 module Items
   # service for various item methods
   class Service
-    ALMA_BASE_URL = 'https://api-na.hosted.exlibrisgroup.com/almaws'
-
     FACULTY_EXPRESS_CODE = 'FacEXP'
     COURTESY_BORROWER_CODE = 'courtesy'
-    STUDENT_GROUP_CODES = %w[undergrad graduate GIC].freeze
-
-    DEFAULT_REQUEST_HEADERS =
-      { "Authorization": "apikey #{Rails.application.credentials.alma_api_key}",
-        "Accept": 'application/json',
-        "Content-Type": 'application/json' }.freeze
 
     class BoundwithError < StandardError; end
 
     # @return [PennItem]
     def self.item_for(mms_id:, holding_id:, item_pid:)
       raise ArgumentError, 'Insufficient identifiers set' unless mms_id && holding_id && item_pid
-
-      item_url = "#{ALMA_BASE_URL}/v1/bibs/#{mms_id}/holdings/#{holding_id}/items/#{item_pid}"
-      response = Faraday.new(
-        url: item_url,
-        headers: DEFAULT_REQUEST_HEADERS
-      ).get
-
-      PennItem.new(JSON.parse(response.body))
+      item = Alma::BibItem.find_one(mms_id: mms_id, holding_id: holding_id, item_pid: item_pid).item
+      PennItem.new(item)
     end
 
     # Return an array of PennItems for a given mms_id and holding_id, fake an item if a holding has no items
@@ -38,7 +24,7 @@ module Items
       item_set = Alma::BibItem.find(mms_id, holding_id: holding_id).items
       return item_set if item_set.present?
 
-      holdings = holdings_for(mms_id: mms_id)
+      holdings = Alma::BibHolding.find_all(mms_id: mms_id)
 
       # TODO: implement boundwith support, see example mms_id: 9920306003503681
       raise BoundwithError if holdings['holding'].blank?
@@ -46,25 +32,10 @@ module Items
       # Fake an item when a holding has no items, ugh
       [PennItem.new({
                       'bib_data' => holdings['bib_data'],
-                      'holding_data' => holdings_for(mms_id: mms_id)['holding']
+                      'holding_data' => holdings['holding']
                                           &.find { |holding| holding['holding_id'] == holding_id },
                       'item_data' => {}
                     })]
-    end
-
-    # We need data from this holding endpoint in order to fake an item. Theoretically, this should return
-    # a "BibHoldingSet" or a "PennHoldingSet" to fall in line with the .items_for method.
-    # @return [Hash]
-    def self.holdings_for(mms_id:)
-      raise ArgumentError, 'Insufficient identifiers set' unless mms_id
-
-      url = "#{ALMA_BASE_URL}/v1/bibs/#{mms_id}/holdings"
-      response = Faraday.new(
-        url: url,
-        headers: DEFAULT_REQUEST_HEADERS
-      ).get
-
-      JSON.parse(response.body)
     end
 
     # @return [Array]
---
+++ config/initializers/alma_bibholding_findall.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# The Alma API gem does not yet support the ability to get all holdings for a bib using the mms_id and the holding_id.
+# This class is a workaround for that limitation.
+
+module AlmaBibHoldingFindAll
+  def find_all(mms_id:, options: {})
+    url = "#{bibs_base_path}/#{mms_id}/holdings"
+    response = HTTParty.get(url, headers:, query: options, timeout:)
+    JSON.parse(response.body)
+  end
+end
+
+Alma::BibHolding.extend(AlmaBibHoldingFindAll)
