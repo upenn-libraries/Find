8940d4fb9cd36194b2117eae8bccd1d857c1723f
Andrew Kimball <andrewnk@upenn.edu>
2024-05-01 11:56:38 -0400

    Set up solr url and chrome url for testing

--- rails_app/spec/rails_helper.rb
+++ rails_app/spec/rails_helper.rb
@@ -8,7 +8,17 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
+
+# Allow connections to localhost, solr, and solr-1 when running tests.
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: [
+    "chrome",
+    "solr",
+    "solr-1"
+  ]
+)
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
