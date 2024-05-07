14cf0f0992d75d75c9b89c835139afe225fba14a
Patrick Perkins <lilblizzard97@gmail.com>
2024-05-07 13:03:57 -0400

    google analytics tags

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,29 @@ import "controllers";
 import "popper";
 import "bootstrap";
 import Blacklight from "blacklight";
+
+const events = [
+  "turbo:fetch-request-error",
+  "turbo:frame-missing",
+  "turbo:frame-load",
+  "turbo:frame-render",
+  "turbo:before-frame-render",
+  "turbo:load",
+  "turbo:render",
+  "turbo:before-stream-render",
+  "turbo:before-render",
+  "turbo:before-cache",
+  "turbo:submit-end",
+  "turbo:before-fetch-response",
+  "turbo:before-fetch-request",
+  "turbo:submit-start",
+  "turbo:visit",
+  "turbo:before-visit",
+  "turbo:click",
+];
+
+events.forEach((e) => {
+  addEventListener(e, () => {
+    console.log(e);
+  });
+});
