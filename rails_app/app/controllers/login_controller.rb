800de540d887af453d021937e483cde886ef506d
Mike Kanning <mkanning@upenn.edu>
2024-05-13 22:56:42 -0400

    remove guard that disallows login in production

--- rails_app/app/controllers/login_controller.rb
+++ rails_app/app/controllers/login_controller.rb
@@ -2,13 +2,7 @@
 
 # handles requests from user login entrypoint
 class LoginController < ApplicationController
-  def index
-    if Rails.env.production?
-      head :not_found
-    else
-      render :index
-    end
-  end
+  def index; end
 
   def alma; end
 end
