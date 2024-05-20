5e2083bdb711f1573accdffb454c8d23136dfb5d
Mike Kanning <mkanning@upenn.edu>
2024-05-20 14:25:11 -0400

    address rubocop violations by refactoring AvailableStatus spec to use a shared example set

--- rails_app/spec/services/inventory/entry/available_status_spec.rb
+++ rails_app/spec/services/inventory/entry/available_status_spec.rb
@@ -2,44 +2,46 @@
 
 describe Inventory::Entry::AvailableStatus do
   subject(:availability_status)  { described_class.new(**location_opts) }
+  let(:scope) { %i[alma availability available physical] << type }
+
+  shared_examples 'has the correct attribute values' do
+    it { expect(availability_status.refined).to eq type }
+    it { expect(availability_status.label).to eq I18n.t(:label, scope: scope) }
+    it { expect(availability_status.description).to eq I18n.t(:description, scope: scope) }
+  end
 
   context 'with an offsite location' do
     let(:location_opts) { { library_code: Inventory::Constants::LIBRA_LIBRARY, location_code: 'stor' } }
+    let(:type) { :offsite }
 
-    it { expect(availability_status.refined).to eq :offsite }
-    it { expect(availability_status.label).to eq I18n.t('alma.availability.available.physical.offsite.label') }
-    it { expect(availability_status.description).to eq I18n.t('alma.availability.available.physical.offsite.description') }
+    include_examples 'has the correct attribute values'
   end
 
   context 'with an Aeon location' do
     let(:location_opts) { { library_code: 'KatzLib', location_code: 'cjsambx' } }
+    let(:type) { :appointment }
 
-    it { expect(availability_status.refined).to eq :appointment }
-    it { expect(availability_status.label).to eq I18n.t('alma.availability.available.physical.appointment.label') }
-    it { expect(availability_status.description).to eq I18n.t('alma.availability.available.physical.appointment.description') }
+    include_examples 'has the correct attribute values'
   end
 
   context 'with an offsite Aeon location' do
     let(:location_opts) { { library_code: Inventory::Constants::LIBRA_LIBRARY, location_code: 'athstor' } }
+    let(:type) { :appointment }
 
-    it { expect(availability_status.refined).to eq :appointment }
-    it { expect(availability_status.label).to eq I18n.t('alma.availability.available.physical.appointment.label') }
-    it { expect(availability_status.description).to eq I18n.t('alma.availability.available.physical.appointment.description') }
+    include_examples 'has the correct attribute values'
   end
 
   context 'with an unrequestable location' do
     let(:location_opts) { { library_code: Inventory::Constants::HSP_LIBRARY, location_code: 'hspclosed' } }
+    let(:type) { :unrequestable }
 
-    it { expect(availability_status.refined).to eq :unrequestable }
-    it { expect(availability_status.label).to eq I18n.t('alma.availability.available.physical.unrequestable.label') }
-    it { expect(availability_status.description).to eq I18n.t('alma.availability.available.physical.unrequestable.description') }
+    include_examples 'has the correct attribute values'
   end
 
   context 'with a circulating location' do
     let(:location_opts) { { library_code: 'vanpelt', location_code: 'vanp' } }
+    let(:type) { :circulates }
 
-    it { expect(availability_status.refined).to eq :circulates }
-    it { expect(availability_status.label).to eq I18n.t('alma.availability.available.physical.circulates.label') }
-    it { expect(availability_status.description).to eq I18n.t('alma.availability.available.physical.circulates.description') }
+    include_examples 'has the correct attribute values'
   end
 end
