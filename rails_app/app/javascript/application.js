c607bd5af404d55d21f712f7e70d5d72ba16b16c
Jon Earley <earleyj@upenn.edu>
2024-05-20 16:42:23 -0400

    Setup of custom element to search options.

--- rails_app/app/javascript/application.js
+++ rails_app/app/javascript/application.js
@@ -4,3 +4,56 @@ import "controllers";
 import "popper";
 import "bootstrap";
 import Blacklight from "blacklight";
+
+class SearchList extends HTMLElement {
+  constructor() {
+    super();
+
+    const LIST_LENGTH_THRESHOLD = 12;
+
+    this.searchableNodes = this.childNodes;
+
+    if (this.childElementCount > LIST_LENGTH_THRESHOLD) {
+      this.addSearch();
+    }
+  }
+
+  connectedCallback() {
+    this.inputEl.addEventListener('input', this.search.bind(this))
+  }
+
+  disconnectedCallback() {
+    this.inputEl.removeEventListener('input')
+  }
+
+  addSearch() {
+    let containerEl = document.createElement('div');
+    containerEl.setAttribute("class", "search-list")
+
+    let inputEl = document.createElement('input');
+    inputEl.setAttribute("type", "text");
+    inputEl.setAttribute("class", "form-control search-list__input")
+    inputEl.setAttribute("placeholder", "Search options");
+    inputEl.setAttribute("aria-label", "Search options");
+
+    containerEl.appendChild(inputEl);
+
+    this.prepend(containerEl);
+    this.inputEl = inputEl;
+  }
+
+  search() {
+    const term = this.inputEl.value.toLowerCase();
+    const elements = this.querySelectorAll('.inventory-item');
+
+    elements.forEach(node => {
+      if (node.textContent.toLowerCase().includes(term) || term.length === 0) {
+        node.removeAttribute('hidden')
+      } else {
+        node.setAttribute('hidden', true)
+      }
+    })
+  }
+}
+
+customElements.define("search-list", SearchList)
