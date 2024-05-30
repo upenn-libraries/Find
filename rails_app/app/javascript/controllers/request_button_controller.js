9ca7415bda151dee71e428fbe24a92599353aa4a
Patrick Perkins <lilblizzard97@gmail.com>
2024-05-30 17:04:41 -0400

    add experimental anchor addition to request button controller

--- rails_app/app/javascript/controllers/request_button_controller.js
+++ rails_app/app/javascript/controllers/request_button_controller.js
@@ -2,13 +2,19 @@ import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
     static targets = [
-        "requestButton"
+        "requestButton",
+        "loginRequestButton",
     ];
 
     connect() {
+        console.log(this.loginRequestButtonTarget);
         // if the current url contains an anchor of 'request_item', open the options frame
         if (window.location.hash === "#request_item") {
             this.requestButtonTarget.setAttribute("open", true);
         }
+    }
+
+    addAnchorToUrl() {
+        window.location.hash = 'request_item';
     }
 }
