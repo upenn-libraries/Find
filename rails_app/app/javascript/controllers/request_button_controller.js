c0cf25224a2fe6fda9115f21298ceef40c6bcebd
Patrick Perkins <lilblizzard97@gmail.com>
2024-05-31 14:31:28 -0400

    wip login button redirect with query params

--- rails_app/app/javascript/controllers/request_button_controller.js
+++ rails_app/app/javascript/controllers/request_button_controller.js
@@ -14,7 +14,21 @@ export default class extends Controller {
         }
     }
 
-    addAnchorToUrl() {
-        window.location.hash = 'request_item';
+    addQueryToUrl(event) {
+        event.preventDefault();
+        let url = new URL(window.location.href);
+
+        // Get the current query parameters
+        let params = new URLSearchParams(url.search);
+
+        // Set the new or update the existing query parameter
+        params.set('request', 'true');
+
+        // Update the URL with the new query parameters
+        url.search = params.toString();
+
+        // Use history.pushState to update the address bar without reloading the page
+        history.pushState({}, '', url);
+        window.location.href = '/login';
     }
 }
