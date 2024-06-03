f004922230c6ddd5b78f9ef5070443ce01330d5a
Patrick Perkins <lilblizzard97@gmail.com>
2024-06-03 12:47:55 -0400

    refactor recursive call, update specs to accommodate total_record_count call

--- rails_app/app/services/inventory/service/physical.rb
+++ rails_app/app/services/inventory/service/physical.rb
@@ -5,67 +5,63 @@ module Inventory
   class Service
     # Retrieves Alma physical item data
     class Physical
-      class BoundwithError < StandardError; end
+      class << self
+        # Get a single Item for a given mms_id, holding_id, and item_pid
+        # @param mms_id [String] the Alma mms_id
+        # @param holding_id [String] the Alma holding_id
+        # @param item_pid [String] the Alma item_pid
+        # @return [PennItem]
+        def item(mms_id:, holding_id:, item_pid:)
+          raise ArgumentError, 'Insufficient identifiers set' unless mms_id && holding_id && item_pid
 
-      # Get a single Item for a given mms_id, holding_id, and item_pid
-      # @params mms_id [String] the Alma mms_id
-      # @params holding_id [String] the Alma holding_id
-      # @params item_pid [String] the Alma item_pid
-      # @return [PennItem]
-      def self.item(mms_id:, holding_id:, item_pid:)
-        raise ArgumentError, 'Insufficient identifiers set' unless mms_id && holding_id && item_pid
+          item = Alma::BibItem.find_one(mms_id: mms_id, holding_id: holding_id, item_pid: item_pid).item
+          Inventory::Service::Item.new(item)
+        end
 
-        item = Alma::BibItem.find_one(mms_id: mms_id, holding_id: holding_id, item_pid: item_pid).item
-        Inventory::Service::Item.new(item)
+        # Return an array of PennItems for a given mms_id and holding_id, fake an item if a holding has no items
+        # @param mms_id [String] the Alma mms_id
+        # @param holding_id [String] the Alma holding_id
+        # @return [Array<PennItem>]
+        def items(mms_id:, holding_id:)
+          raise ArgumentError, 'Insufficient identifiers set' unless mms_id && holding_id
+
+          item_set = fetch_all_items(mms_id: mms_id, holding_id: holding_id)
+          return item_set if item_set.present?
+
+          holdings = Alma::BibHolding.find_all(mms_id: mms_id)
+
+          # TODO: implement boundwith support, see example mms_id: 9920306003503681
+          return [] if holdings['items'].blank?
+
+          # Fake an item when a holding has no items, ugh
+          [Inventory::Service::Item.new({
+                                          'bib_data' => holdings['bib_data'],
+                                          'holding_data' => holdings['holding']
+                                                              &.find { |holding| holding['holding_id'] == holding_id },
+                                          'item_data' => {}
+                                        })]
+        end
+
+        private
+
+        # Recursively fetch all items for a given mms_id and holding_id
+        # @param mms_id [String]
+        # @param holding_id [String]
+        # @param limit [Integer]
+        # @param offset [Integer]
+        # @param accumulated_items [Array<PennItem>]
+        def fetch_all_items(mms_id:, holding_id:, limit: 100, offset: 0, accumulated_items: [])
+          response = Alma::BibItem.find(mms_id, holding_id: holding_id, limit: limit, offset: offset)
+          accumulated_items += response.items
+
+          # Base case: if the number of items returned is less than the limit, we've fetched all items
+          return accumulated_items if response.total_record_count == accumulated_items.count
+
+          # Recursive case: fetch the next batch of items
+          fetch_all_items(mms_id: mms_id, holding_id: holding_id, limit: limit,
+                          offset: offset + limit, accumulated_items: accumulated_items)
+        end
       end
-
-      # Return an array of PennItems for a given mms_id and holding_id, fake an item if a holding has no items
-      # @params mms_id [String] the Alma mms_id
-      # @params holding_id [String] the Alma holding_id
-      # @return [Array<PennItem>]
-      def self.items(mms_id:, holding_id:)
-        raise ArgumentError, 'Insufficient identifiers set' unless mms_id && holding_id
-
-        item_set = fetch_all_items(mms_id: mms_id, holding_id: holding_id)
-        return item_set if item_set.present?
-
-        holdings = Alma::BibHolding.find_all(mms_id: mms_id)
-
-        # TODO: implement boundwith support, see example mms_id: 9920306003503681
-        # raise BoundwithError if holdings['holding'].blank?
-        return [] if holdings['items'].blank?
-
-        # Fake an item when a holding has no items, ugh
-        [Inventory::Service::Item.new({
-                                        'bib_data' => holdings['bib_data'],
-                                        'holding_data' => holdings['holding']
-                                                            &.find { |holding| holding['holding_id'] == holding_id },
-                                        'item_data' => {}
-                                      })]
-      end
-
-      # Recursively fetch all items for a given mms_id and holding_id
-      # @param mms_id [String]
-      # @param holding_id [String]
-      # @param limit [Integer]
-      # @param offset [Integer]
-      # @param accumulated_items [Array<PennItem>]
-      def self.fetch_all_items(mms_id:, holding_id:, limit: 100, offset: 0, accumulated_items: [])
-        items = Alma::BibItem.find(mms_id, holding_id: holding_id, limit: limit, offset: offset).items
-        accumulated_items += items
-
-        # Base case: if the number of items returned is less than the limit, we've fetched all items
-        return accumulated_items if items.size < limit
-
-        # Recursive case: fetch the next batch of items
-        fetch_all_items(mms_id: mms_id, holding_id: holding_id, limit: limit,
-                        offset: offset + limit, accumulated_items: accumulated_items)
-      rescue BibItemSet::ResponseError
-        # If the total count of items is a multiple of 100, we'll get a ResponseError when calling for the next batch.
-        # I don't love that this error is so non-specific, but this is a limitation of the Alma gem and the Alma API.
-        accumulated_items
-      end
-      private_class_method :fetch_all_items
     end
   end
 end
--- rails_app/spec/services/inventory/service/physical_spec.rb
+++ rails_app/spec/services/inventory/service/physical_spec.rb
@@ -23,15 +23,24 @@ describe Inventory::Service::Physical do
 
     it 'returns an array of PennItems when items are present' do
       bib_item_set_double = instance_double(Alma::BibItemSet, items: [item])
+      allow(bib_item_set_double).to receive(:total_record_count).and_return(1)
       allow(Alma::BibItem).to receive(:find).and_return(bib_item_set_double)
       expect(described_class.items(mms_id: '123', holding_id: '456')).to all(be_a Inventory::Service::Item)
     end
 
     it 'returns an array of PennItems when items are not present' do
       bib_item_set_double = instance_double(Alma::BibItemSet, items: [])
+      allow(bib_item_set_double).to receive(:total_record_count).and_return(1)
       allow(Alma::BibItem).to receive(:find).and_return(bib_item_set_double)
       allow(Alma::BibHolding).to receive(:find_all).and_return('holding' => [{ 'holding_id' => '456' }])
       expect(described_class.items(mms_id: '123', holding_id: '456')).to all(be_a Inventory::Service::Item)
+    end
+
+    it 'returns all items when items exceed limit' do
+      # mock bibitemset before going to factory
+      # test that first request returns a total count of 4 (create a set)
+      # make sure that fetch_all_items is called twice (or that the alma api is called twice)
+      # make sure that count of pennitems is 4
     end
 
     it 'raises an ArgumentError if a parameter is missing' do
