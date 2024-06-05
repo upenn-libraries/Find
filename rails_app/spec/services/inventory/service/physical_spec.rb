d0431e8f60d4f443265f02e0d1a06ca542c071ef
Patrick Perkins <lilblizzard97@gmail.com>
2024-06-05 12:09:15 -0400

    simplify total record count stub

--- rails_app/spec/services/inventory/service/physical_spec.rb
+++ rails_app/spec/services/inventory/service/physical_spec.rb
@@ -22,23 +22,20 @@ describe Inventory::Service::Physical do
     let(:item) { build :item }
 
     it 'returns an array of PennItems when items are present' do
-      bib_item_set_double = instance_double(Alma::BibItemSet, items: [item])
-      allow(bib_item_set_double).to receive(:total_record_count).and_return(1)
+      bib_item_set_double = instance_double(Alma::BibItemSet, items: [item], total_record_count: 1)
       allow(Alma::BibItem).to receive(:find).and_return(bib_item_set_double)
       expect(described_class.items(mms_id: '123', holding_id: '456').first).to be_a Inventory::Service::Item
     end
 
     it 'returns an array of PennItems when items are not present' do
-      bib_item_set_double = instance_double(Alma::BibItemSet, items: [])
-      allow(bib_item_set_double).to receive(:total_record_count).and_return(0)
+      bib_item_set_double = instance_double(Alma::BibItemSet, items: [], total_record_count: 0)
       allow(Alma::BibItem).to receive(:find).and_return(bib_item_set_double)
       allow(Alma::BibHolding).to receive(:find_all).and_return('holding' => [{ 'holding_id' => '456' }])
       expect(described_class.items(mms_id: '123', holding_id: '456').first).to be_a Inventory::Service::Item
     end
 
     it 'returns an empty array when holding data is blank' do
-      bib_item_set_double = instance_double(Alma::BibItemSet, items: [])
-      allow(bib_item_set_double).to receive(:total_record_count).and_return(0)
+      bib_item_set_double = instance_double(Alma::BibItemSet, items: [], total_record_count: 0)
       allow(Alma::BibItem).to receive(:find).and_return(bib_item_set_double)
       allow(Alma::BibHolding).to receive(:find_all).and_return({})
       expect(described_class.items(mms_id: '123', holding_id: '456')).to eq []
@@ -51,8 +48,7 @@ describe Inventory::Service::Physical do
 
   describe '.fetch_all_items' do
     it 'makes multiple calls to Alma when total record count exceeds limit' do
-      bib_item_set_double = instance_double(Alma::BibItemSet, items: build_list(:item, 2))
-      allow(bib_item_set_double).to receive(:total_record_count).and_return(4)
+      bib_item_set_double = instance_double(Alma::BibItemSet, items: build_list(:item, 2), total_record_count: 4)
       allow(Alma::BibItem).to receive(:find).and_return(bib_item_set_double)
 
       described_class.send(:fetch_all_items, mms_id: '1234', holding_id: '1234', limit: 2)
