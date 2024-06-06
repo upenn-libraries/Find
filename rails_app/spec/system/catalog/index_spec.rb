a566b359deb2e342f2994521e812f91e221b32bb
Amrey Mathurin <mamrey@upenn.edu>
2024-06-06 10:47:34 -0400

    Test recently published/added facets

--- rails_app/spec/system/catalog/index_spec.rb
+++ rails_app/spec/system/catalog/index_spec.rb
@@ -97,4 +97,178 @@ describe 'Catalog Index Page' do
       expect(page).to have_selector 'article.document', count: 1
     end
   end
+
+  context 'when viewing the recently published facet', skip: 'until new configset with date fields' do
+    let(:solr_time) { nil }
+
+    before do
+      CatalogController.blacklight_config.default_solr_params = { qt: 'search', NOW: solr_time }
+      visit search_catalog_path
+    end
+
+    context 'without a recently published record' do
+      # the print monograph has a publication date of 1961
+      # the electronic journal has a publication date of 1869
+
+      it ' does not show recently published facet' do
+        within('#facets') do
+          expect(page).not_to have_text I18n.t('facets.recently_published.label')
+        end
+      end
+    end
+
+    context 'with a record published within the last 5 years' do
+      # we calculate the last 5 years as the beginning of the year, 4 years ago.
+
+      let(:solr_time) { (Time.new(1965).to_f * 1000).to_i }
+
+      it 'shows recently published for 5 year range' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).to have_text I18n.t('facets.recently_published.5_years')
+        end
+      end
+
+      it 'shows the recently published facet for 10 and 15 year ranges' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).to have_text I18n.t('facets.recently_published.10_years')
+          expect(page).to have_text I18n.t('facets.recently_published.15_years')
+        end
+      end
+
+      it 'shows the expected facet count' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).to have_text(/\b1\b/, count: 3)
+        end
+      end
+    end
+
+    context 'with a record published within the last 10 years' do
+      # we calculate the last 10 years as the beginning of the year, 9 years ago.
+
+      let(:solr_time) { (Time.new(1970).to_f * 1000).to_i }
+
+      it ' does not show recently published facet for 5 year range' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).not_to have_text I18n.t('facets.recently_published.5_years')
+        end
+      end
+
+      it 'shows recently published facets for 10 and 15 year ranges' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).to have_text I18n.t('facets.recently_published.10_years')
+          expect(page).to have_text I18n.t('facets.recently_published.15_years')
+        end
+      end
+
+      it 'shows the expected facet count' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).to have_text(/\b1\b/, count: 2)
+        end
+      end
+    end
+
+    context 'with a record published within the last 15 years' do
+      # we calculate the last 10 years as the beginning of the year, 14 years ago.
+
+      let(:solr_time) { (Time.new(1975).to_f * 1000).to_i }
+
+      it ' does not show recently published facet for 5 and 10 year ranges' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).not_to have_text I18n.t('facets.recently_published.5_years')
+          expect(page).not_to have_text I18n.t('facets.recently_published.10_years')
+        end
+      end
+
+      it 'shows recently published facets the 15 year range' do
+        within('div.blacklight-recently_published_facet') do
+          click_on I18n.t('facets.recently_published.label')
+          expect(page).to have_text I18n.t('facets.recently_published.15_years')
+          expect(page).to have_text(/\b1\b/, count: 1)
+        end
+      end
+    end
+  end
+
+  context 'when viewing the recently added facet', skip: 'until new configset with date fields' do
+    let(:recently_added_bib) { '9979413181503681' }
+    let(:entries) { [create(:physical_entry, mms_id: recently_added_bib)] }
+    let(:solr_time) { nil }
+
+    before do
+      # added date of "2024-05-30T00:00:00Z"
+      SampleIndexer.index 'recently_added.json'
+
+      allow(Inventory::Service).to receive(:full).with(satisfy { |d| d.fetch(:id) == recently_added_bib })
+                                                 .and_return(Inventory::Response.new(entries: entries))
+      allow(Inventory::Service).to receive(:brief).with(satisfy { |d| d.fetch(:id) == recently_added_bib })
+                                                  .and_return(Inventory::Response.new(entries: entries))
+
+      CatalogController.blacklight_config.default_solr_params = { qt: 'search', NOW: solr_time }
+
+      visit search_catalog_path
+    end
+
+    context 'without a recently added record' do
+      let(:solr_time) { (Time.new(2025).to_f * 1000).to_i }
+
+      it 'does not show the recently added facet' do
+        within('#facets') do
+          expect(page).not_to have_text I18n.t('facets.recently_added.30_days')
+        end
+      end
+    end
+
+    context 'with a record added in the last 15 days' do
+      let(:solr_time) { (Time.new('2024-06-01').to_f * 1000).to_i }
+
+      it 'shows the recently added facet for 15 and 30 day range' do
+        within('div.blacklight-recently_added_facet') do
+          click_on I18n.t('facets.recently_added.label')
+          expect(page).to have_text I18n.t('facets.recently_added.15_days')
+          expect(page).to have_text I18n.t('facets.recently_added.30_days')
+        end
+      end
+
+      it 'shows the recently added facet for 60 and 90 day range' do
+        within('div.blacklight-recently_added_facet') do
+          click_on I18n.t('facets.recently_added.label')
+          expect(page).to have_text I18n.t('facets.recently_added.60_days')
+          expect(page).to have_text I18n.t('facets.recently_added.90_days')
+        end
+      end
+
+      it 'shows the expected facet count' do
+        within('div.blacklight-recently_added_facet') do
+          click_on I18n.t('facets.recently_added.label')
+          expect(page).to have_text(/\b1\b/, count: 4)
+        end
+      end
+    end
+
+    context 'with a record added in the last 90 days' do
+      let(:solr_time) { (Time.new('2024-07-31').to_f * 1000).to_i }
+
+      it ' does not show the recently added facet for smaller date ranges' do
+        within('div.blacklight-recently_added_facet') do
+          click_on I18n.t('facets.recently_added.label')
+          expect(page).not_to have_text(/ Within 15|30|60 days/)
+        end
+      end
+
+      it 'shows the recently added facet for 90 day range' do
+        within('div.blacklight-recently_added_facet') do
+          click_on I18n.t('facets.recently_added.label')
+          expect(page).to have_text I18n.t('facets.recently_added.90_days')
+          expect(page).to have_text('1', count: 1)
+        end
+      end
+    end
+  end
 end
