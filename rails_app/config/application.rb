adc8c58c342c565e9556c7edc00747c57a6fa3d4
Patrick Perkins <lilblizzard97@gmail.com>
2024-06-10 14:56:22 -0400

    load 7.1 defaults

--- rails_app/config/application.rb
+++ rails_app/config/application.rb
@@ -24,7 +24,7 @@ module Find
   # Application object - holding config
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
