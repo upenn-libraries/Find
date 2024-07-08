bd388ea2d7e3ad9296e648a34eab49702f32d9ff
Patrick Perkins <lilblizzard97@gmail.com>
2024-07-08 16:11:10 -0400

    add button to hide comments area

--- rails_app/app/javascript/controllers/comments_controller.js
+++ rails_app/app/javascript/controllers/comments_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["addComments", "commentsArea"];
+  static targets = ["addComments", "commentsArea", "addCommentsButton", "hideCommentsButton"];
   COMMENT_OPTIONS = ["pickup", "ill_pickup", "mail", "office"];
 
   toggleComments({ detail: { option_value } }) {
@@ -15,6 +15,14 @@ export default class extends Controller {
   showCommentsArea(event) {
     event.preventDefault();
     event.target.classList.add("d-none");
+    this.hideCommentsButtonTarget.classList.remove("d-none");
     this.commentsAreaTarget.classList.remove("d-none");
+  }
+
+  hideCommentsArea(event) {
+    event.preventDefault();
+    event.target.classList.add("d-none");
+    this.addCommentsButtonTarget.classList.remove("d-none");
+    this.commentsAreaTarget.classList.add("d-none");
   }
 }
