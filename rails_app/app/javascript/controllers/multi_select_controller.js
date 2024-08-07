c8321cc0c2915082aaab46a3c925359fd2180091
Patrick Perkins <lilblizzard97@gmail.com>
2024-08-07 11:58:23 -0400

    add some additional comments about custom item rendering

--- rails_app/app/javascript/controllers/multi_select_controller.js
+++ rails_app/app/javascript/controllers/multi_select_controller.js
@@ -9,6 +9,9 @@ export default class extends Controller {
   connect() {
     this.select = new TomSelect(this.element, {
       plugins: this.pluginsValue,
+      // Passing the `item` function to the `render` arg allows us to customize what the selected item looks like when it's
+      // added to the list of selected items. In this case, we're just wrapping the value (coming from the data set) in a
+      // div to remove the count that's displayed in the option list.
       render: {
         item: function (data, escape) {
           return `<div>${escape(data.value)}</div>`;
