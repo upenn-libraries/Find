aa6fa5228c951bd48e735f011131c18636a1ecbd
baowei <baowei@upenn.edu>
2024-08-08 12:53:46 -0400

    New method to_ris to convert the format facets to RIS string.

--- rails_app/app/models/concerns/ris_export.rb
+++ rails_app/app/models/concerns/ris_export.rb
@@ -8,16 +8,29 @@ module RisExport
   # Defines the fields of RIS format
   module ClassMethods
     def ris_fields
-      {
-        TY: proc { marc(:format_ris) }, # format
+      { TY: proc { to_ris(marc(:format_facet)) }, # format
         TI: proc { marc(:title_show) }, # title
         AU: proc { marc(:creator_authors_list, main_tags_only: true) }, # author
         PY: proc { marc(:date_publication).year }, # publication year
         CY: proc { marc(:production_publication_ris_place_of_pub) }, # place of publication
         PB: proc { marc(:production_publication_ris_publisher) }, # publisher
         ET: proc { marc(:edition_show, with_alternate: false) }, # edition
-        SN: proc { marc(:identifier_isbn_show) } # ISBN
-      }
+        SN: proc { marc(:identifier_isbn_show) } } # ISBN
+    end
+  end
+
+  private
+
+  # Return RIS format in one of these three values: "BOOK", "JOUR", "GEN"
+  # @param [Array<String>] formats
+  # @return [String]
+  def to_ris(formats)
+    if formats.include? PennMARC::Format::BOOK
+      'BOOK'
+    elsif formats.include? PennMARC::Format::JOURNAL_PERIODICAL
+      'JOUR'
+    else
+      'GEN'
     end
   end
 end
