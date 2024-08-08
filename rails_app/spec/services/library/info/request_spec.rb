449c652912a27325a2ce308ee36b5b811350c60a
Alyssa Hogan <abhogan@upenn.edu>
2024-08-08 11:30:15 -0400

    Add spec for empty library info response set

--- rails_app/spec/services/library/info/request_spec.rb
+++ rails_app/spec/services/library/info/request_spec.rb
@@ -7,11 +7,19 @@ describe Library::Info::Request do
   let(:response_body) { build(:library_info_api_request_response, :with_all_info) }
 
   describe '.find' do
-    context 'with a successful api request' do
+    context 'with a successful api request and library data' do
       before { stub_library_info_api_request_success(library_code: code, response_body: response_body) }
 
       it 'returns a Library::Info::Request object' do
         expect(described_class.find(library_code: code)).to be_a(described_class)
+      end
+    end
+
+    context 'with a successful api request but no available library data' do
+      before { stub_library_info_api_request_success(library_code: code, response_body: []) }
+
+      it 'returns nil' do
+        expect(described_class.find(library_code: code)).to be_nil
       end
     end
 
