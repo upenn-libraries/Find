d072f9da578f2f09b46cfb4648c703f7b63cca30
Carla Galarza <cgalarza@upenn.edu>
2024-08-20 13:41:28 -0400

    Fixing rubocop offense

--- rails_app/app/controllers/fulfillment_controller.rb
+++ rails_app/app/controllers/fulfillment_controller.rb
@@ -10,8 +10,8 @@ class FulfillmentController < ApplicationController
                                      host_record_id: params[:host_record_id],
                                      location_code: params[:location_code])
 
-    # Ensuring we send holding identifier to the fulfillment form in cases where a holding identifier is not already present.
-    # Holding identifiers are not received from the availability api if an item is in a temporary location.
+    # Ensuring we send holding identifier to the fulfillment form in cases where a holding identifier is not already
+    # present. Holding identifiers are not received from the availability api if an item is in a temporary location.
     render(Fulfillment::FormComponent.new(mms_id: params[:mms_id],
                                           holding_id: params[:holding_id] || items.first.holding_id,
                                           items: items,
