b122132148f181ef518b201175a303d82d2ba126
Mike Kanning <mkanning@upenn.edu>
2024-08-29 13:59:54 -0400

    fix email method on Fulfillment::User. add spec

--- rails_app/spec/services/fulfillment/user_spec.rb
+++ rails_app/spec/services/fulfillment/user_spec.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
 describe Fulfillment::User do
+  include Alma::ApiMocks::User
+
+  let(:uid) { 'test' }
+
   it_behaves_like 'Illiad Account' do
-    let(:object) { described_class.new(uid: 'test') }
+    let(:object) { described_class.new(uid) }
+  end
+
+  describe '#email' do
+    let(:email) { "#{uid}@upenn.edu" }
+
+    before do
+      stub_alma_user_find_success(id: uid, response_body: {
+                                    'contact_info' => { 'email' => [{ 'email_address' => email,
+                                                                      'preferred' => true }] }
+                                  })
+    end
+
+    it 'has an email' do
+      expect(described_class.new(uid).email).to eq email
+    end
   end
 end
