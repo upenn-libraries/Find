17bb97efdce44806241a719af139948f20a5a9ea
Mike Kanning <mkanning@upenn.edu>
2024-08-29 12:25:48 -0400

    properly join multiple note values in the PhysicalDetailComponent

--- rails_app/spec/requests/inventory_controller_spec.rb
+++ rails_app/spec/requests/inventory_controller_spec.rb
@@ -31,7 +31,7 @@ describe 'Inventory requests' do
       before { get physical_detail_inventory_path(print_monograph_bib, print_monograph_entries.second.id) }
 
       it 'returns expected note' do
-        expect(response.body).to include 'First note Second note Third note2-First note 2-Second note 2-Third note'
+        expect(response.body).to include 'First note Second note Third note 2-First note 2-Second note 2-Third note'
       end
     end
   end
