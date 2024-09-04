2c745626dd3abc438490ede52aeba56753ba6444
Patrick Perkins <lilblizzard97@gmail.com>
2024-09-04 12:36:29 -0400

    unlimit facet select options with maxOptions config value

--- rails_app/app/javascript/controllers/multi_select_controller.js
+++ rails_app/app/javascript/controllers/multi_select_controller.js
@@ -9,6 +9,8 @@ export default class extends Controller {
   connect() {
     this.select = new TomSelect(this.element, {
       plugins: this.pluginsValue,
+      // Default is 50, we want unlimited to display all options
+      maxOptions: null,
       // Passing the `item` function to the `render` arg allows us to customize what the selected item looks like when it's
       // added to the list of selected items. In this case, we're just wrapping the value (coming from the data set) in a
       // div to remove the count that's displayed in the option list.
