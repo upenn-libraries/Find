3806eb6e50d61b4443606e5036e79bb5b3d3df13
baowei <baowei@upenn.edu>
2024-11-06 10:16:15 -0500

    Adjust the fields

--- rails_app/app/controllers/catalog_controller.rb
+++ rails_app/app/controllers/catalog_controller.rb
@@ -181,13 +181,17 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field :creator_ss,
-                           label: I18n.t('results.creator'), component: Catalog::FacetLinkComponent,
-                           facet_target: :creator_facet, facet_map: :creator_show_facet_map
+
+    # this field above all the clickable fields
+    config.add_show_field :format_facet, label: I18n.t('results.format')
+
     config.add_show_field :creator_show,
                           label: I18n.t('show.creator.main'), accessor: :marc,
                           component: Catalog::FacetLinkComponent, facet_target: :creator_facet,
                           facet_map: :creator_show_facet_map
+    config.add_show_field :creator_contributor_show,
+                          label: I18n.t('show.creator.contributor'), accessor: :marc,
+                          component: Catalog::FacetLinkComponent, facet_target: :creator_facet
     config.add_show_field :creator_conference_detail_show,
                           label: I18n.t('show.creator.conference_detail'),
                           accessor: :marc, component: Catalog::FacetLinkComponent, facet_target: :creator_facet,
@@ -208,10 +212,10 @@ class CatalogController < ApplicationController
     config.add_show_field :subject_local_show,
                           label: I18n.t('show.subject.local'), accessor: :marc,
                           component: Catalog::FacetLinkComponent, facet_target: :subject_facet
-    config.add_show_field :creator_contributor_show,
-                          label: I18n.t('show.creator.contributor'), accessor: :marc,
-                          component: Catalog::FacetLinkComponent, facet_target: :creator_facet
 
+    config.add_index_field :creator_ss,
+                           label: I18n.t('results.creator'), component: Catalog::FacetLinkComponent,
+                           facet_target: :creator_facet, facet_map: :creator_show_facet_map
     config.add_index_field :title_alternate_show, accessor: :marc
     config.add_index_field :score, label: I18n.t('results.score'), if: :show_score?, helper_method: :as_badge
     config.add_index_field :format_facet, label: I18n.t('results.format')
@@ -239,7 +243,6 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field :format_facet, label: I18n.t('results.format')
     config.add_show_field :format_show, label: I18n.t('show.format.main'), accessor: :marc
     config.add_show_field :edition_show, label: I18n.t('show.edition.main'), accessor: :marc
     config.add_show_field :production_show, label: I18n.t('show.production.main'), accessor: :marc
