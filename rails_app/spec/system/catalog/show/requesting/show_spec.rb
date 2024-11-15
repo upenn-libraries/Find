25037fe17c8a2750d45cd8ffd9941f0c6f638211
Mike Kanning <mkanning@upenn.edu>
2024-11-15 13:32:31 -0500

    refactor show page system spec as it was approaching 1000 lines

---
+++ rails_app/spec/system/catalog/show/requesting/show_spec.rb
@@ -0,0 +1,202 @@
+# frozen_string_literal: true
+
+require 'system_helper'
+
+describe 'Catalog show page requesting behaviors' do
+  include_context 'with print monograph record with 2 physical entries'
+
+  let(:user) { create(:user) }
+  let(:mms_id) { print_monograph_bib }
+
+  context 'when logged in' do
+    include_context 'with mocked illiad_record on user'
+
+    let(:entries) { print_monograph_entries }
+
+    before do
+      sign_in user
+      visit solr_document_path(mms_id)
+      click_button entries.second.description
+    end
+
+    it 'shows the button to request the item' do
+      within('details.fulfillment') do
+        expect(page).to have_selector 'summary', text: I18n.t('requests.form.request_item')
+      end
+    end
+
+    context 'when holding is a boundwith' do
+      let(:item) { build :item, :boundwith }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return([item])
+        find('details.fulfillment > summary').click
+      end
+
+      it 'shows boundwith notice' do
+        expect(page).to have_text I18n.t('requests.form.options.boundwith')
+      end
+    end
+
+    context 'with a holding that has multiple checkoutable items' do
+      let(:items) { build_list :item, 2, :checkoutable }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return(items)
+        allow(Inventory::Item).to receive(:find).and_return(items.first)
+        find('details.fulfillment > summary').click
+      end
+
+      it 'shows the item dropdown when there are more than one item' do
+        expect(page).to have_selector 'select#item_id'
+      end
+
+      it 'shows request options when an item is selected' do
+        find('select#item_id').find(:option, items.first.description).select_option
+        expect(page).to have_selector '#delivery-options'
+      end
+    end
+
+    context 'when adding comments to a request' do
+      let(:item) { build :item, :checkoutable }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return([item])
+        find('details.fulfillment > summary').click
+        find('input#delivery_pickup').click
+      end
+
+      it 'shows a button to add comments when the option is changed from scan' do
+        within('#add-comments') do
+          expect(page).to have_link I18n.t('requests.form.add_comments')
+        end
+      end
+
+      it 'hides the comments area when the option is changed back to scan' do
+        find('input#delivery_electronic').click
+        within('form.fulfillment-form') do
+          expect(page).not_to have_selector '#add-comments'
+        end
+      end
+
+      it 'expands the comments area when the button is clicked' do
+        click_link I18n.t('requests.form.add_comments')
+        within('#add-comments') do
+          expect(page).to have_selector 'textarea#comments'
+        end
+      end
+    end
+
+    context 'with an item that is unavailable' do
+      let(:item) { build :item, :not_checkoutable }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return([item])
+        find('details.fulfillment > summary').click
+      end
+
+      it 'shows a note about the unavailability status' do
+        within('.fulfillment__container') do
+          expect(page).to have_content I18n.t('requests.form.options.only_ill_requestable')
+        end
+      end
+
+      it 'shows request options' do
+        within('.fulfillment__container') do
+          expect(page).to have_selector '#delivery-options'
+        end
+      end
+
+      it 'selects the first option' do
+        within('#delivery-options') do
+          expect(first('input[type="radio"]')[:checked]).to be true
+        end
+      end
+
+      it 'shows the right button' do
+        within('.request-buttons') do
+          expect(page).to have_link I18n.t('requests.form.buttons.scan')
+        end
+      end
+
+      context 'when user a courtesy borrower' do
+        let(:user) { create(:user, :courtesy_borrower) }
+
+        it 'shows message saying the item is unavailable' do
+          expect(page).to have_content I18n.t('requests.form.options.none.info')
+        end
+      end
+    end
+  end
+
+  context 'when not logged in' do
+    before do
+      visit solr_document_path(mms_id)
+      click_button print_monograph_entries.first.description
+    end
+
+    context 'with an aeon requestable item' do
+      let(:print_monograph_entries) do
+        [create(:physical_entry, mms_id: print_monograph_bib, holding_id: '1234', location_code: 'scrare')]
+      end
+      let(:item) { build :item, :aeon_requestable }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return([item])
+        find('details.fulfillment > summary').click
+      end
+
+      it 'shows the aeon request options' do
+        within('.fulfillment__container') do
+          expect(page).to have_selector '#aeon-option'
+        end
+      end
+
+      it 'shows the schedule visit button with aeon href' do
+        within('.request-buttons') do
+          aeon_link = find_link I18n.t('requests.form.buttons.aeon')
+          expect(aeon_link[:href]).to start_with(Settings.aeon.requesting_url)
+          expect(aeon_link[:href]).to include(CGI.escape(item.bib_data['title']))
+        end
+      end
+    end
+
+    context 'with an item at the archives' do
+      let(:print_monograph_entries) do
+        [create(:physical_entry, mms_id: print_monograph_bib, holding_id: '1234',
+                                 library_code: Inventory::Location::ARCHIVES)]
+      end
+      let(:item) { build :item, :at_archives }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return([item])
+        find('details.fulfillment > summary').click
+      end
+
+      it 'shows the archives text' do
+        within('.fulfillment__container') do
+          expect(page).to have_selector '#archives-option'
+        end
+      end
+    end
+
+    context 'with an item at HSP' do
+      let(:print_monograph_entries) do
+        [create(:physical_entry, mms_id: print_monograph_bib, holding_id: '1234',
+                                 library_code: Inventory::Location::HSP)]
+      end
+      let(:item) { build :item, :at_hsp }
+
+      before do
+        allow(Inventory::Item).to receive(:find_all).and_return([item])
+        find('details.fulfillment > summary').click
+      end
+
+      it 'shows the archives text' do
+        within('.fulfillment__container') do
+          expect(page).to have_selector '#hsp-option'
+        end
+      end
+    end
+  end
+end
---
+++ rails_app/spec/system/catalog/show/show_spec.rb
@@ -0,0 +1,233 @@
+# frozen_string_literal: true
+
+require 'system_helper'
+
+describe 'Catalog Show Page' do
+  context 'when the Alma API calls time out' do
+    include_context 'with electronic database record having a resource link entry but the Alma API times out'
+
+    before do
+      visit solr_document_path(electronic_db_bib)
+    end
+
+    it 'renders the page with appropriate message' do
+      expect(page).to have_text I18n.t('inventory.incomplete_inventory_html')
+    end
+
+    it 'still shows any resource link holdings' do
+      expect(page).to have_link 'Connect to resource', href: 'http://hdl.library.upenn.edu/1017/126017'
+    end
+  end
+
+  context 'when interacting with show tools' do
+    include_context 'with electronic journal record with 4 electronic entries'
+
+    let(:mms_id) { electronic_journal_bib }
+
+    before { visit solr_document_path(mms_id) }
+
+    context 'when a user is signed in' do
+      before do
+        sign_in create(:user)
+        visit solr_document_path(mms_id)
+      end
+
+      it 'displays a link to email the record' do
+        click_on I18n.t('blacklight.tools.title')
+        expect(page).to have_link 'Email', href: email_solr_document_path(mms_id)
+      end
+
+      # rubocop:disable RSpec/ExampleLength
+      it 'properly sends an email' do
+        expect {
+          click_on I18n.t('blacklight.tools.title')
+          click_on I18n.t('blacklight.tools.email')
+          fill_in :to, with: 'patron@upenn.edu'
+          click_on I18n.t('blacklight.email.form.submit')
+        }.to change(ActionMailer::Base.deliveries, :count).by(1)
+      end
+      # rubocop:enable RSpec/ExampleLength
+    end
+
+    context 'when a user is not signed in' do
+      it 'displays a link to login' do
+        click_on I18n.t('blacklight.tools.title')
+        expect(page).to have_link(I18n.t('blacklight.tools.login_for_email'), href: login_path(id: mms_id))
+      end
+    end
+
+    it 'displays Staff view link' do
+      click_on I18n.t('blacklight.tools.title')
+      expect(page).to have_link I18n.t('blacklight.tools.staff_view'), href: staff_view_solr_document_path(mms_id)
+    end
+  end
+
+  context 'when viewing inventory navigation pane' do
+    include_context 'with print monograph record with 2 physical entries'
+
+    before { visit(solr_document_path(print_monograph_bib)) }
+
+    it 'applies the correct class when holding is available' do
+      expect(page).to have_button(class: 'inventory-item__availability--easy')
+    end
+
+    context 'when holdings have an availability status other than "available"' do
+      include_context 'with print monograph record with 2 physical entries' do
+        let(:print_monograph_entries) do
+          [create(:physical_entry, mms_id: print_monograph_bib, availability: 'unavailable', holding_id: '1234',
+                                   call_number: 'Oversize QL937 B646 1961', holding_info: 'first copy',
+                                   location_code: 'veteresov', inventory_type: 'physical'),
+           create(:physical_entry, mms_id: print_monograph_bib, availability: 'check holdings', holding_id: '5678',
+                                   call_number: 'Oversize QL937 B646 1961 copy 2', holding_info: 'second copy',
+                                   location_code: 'veteresov', inventory_type: 'physical')]
+        end
+      end
+
+      it 'applies the correct class when availability status is "unavailable"' do
+        within('#inventory-pills-tab') do
+          expect(page).to have_button(class: 'inventory-item__availability--difficult')
+        end
+      end
+
+      it 'applies the correct class when availability status is "check holdings"' do
+        within('#inventory-pills-tab') do
+          expect(page).to have_button(class: 'inventory-item__availability')
+        end
+      end
+    end
+  end
+
+  context 'when linking to a facet search' do
+    context 'when no matching facet is found' do
+      include_context 'with print monograph record with 2 physical entries'
+
+      before do
+        CatalogController.configure_blacklight do |config|
+          config.add_show_field :subject_test_show, values: ->(_, _, _) { ['Dogs.'] },
+                                                    component: Catalog::FacetLinkComponent
+        end
+
+        visit(solr_document_path(print_monograph_bib))
+      end
+
+      it 'shows the display value without a link to facet search' do
+        within('.col-md-9.blacklight-subject_test_show') do
+          expect(page).to have_text('Dogs.')
+          expect(page).not_to have_link('Dogs.')
+        end
+      end
+    end
+
+    context 'when viewing main creator' do
+      include_context 'with print monograph record with 2 physical entries'
+
+      before { visit(solr_document_path(print_monograph_bib)) }
+
+      it 'links to creator facet search' do
+        within('.col-md-9.blacklight-creator_show') do
+          expect(page).to have_link('Bleier, Ruth, 1923-',
+                                    href: search_catalog_path({ 'f[creator_facet][]': 'Bleier, Ruth, 1923-' }))
+        end
+      end
+    end
+
+    context 'when viewing subjects' do
+      include_context 'with print monograph record with 2 physical entries'
+
+      before { visit(solr_document_path(print_monograph_bib)) }
+
+      it 'links to a subject facet search' do
+        within('.col-md-9.blacklight-subject_show') do
+          expect(page).to have_link 'Cats.', href: search_catalog_path({ 'f[subject_facet][]': 'Cats' })
+          expect(page).to have_link 'Hypothalamus.', href: search_catalog_path({ 'f[subject_facet][]': 'Hypothalamus' })
+        end
+      end
+    end
+
+    context 'when viewing medical subjects' do
+      include_context 'with print monograph record with 2 physical entries'
+
+      before { visit(solr_document_path(print_monograph_bib)) }
+
+      it 'links to a subject facet search' do
+        within('.col-md-9.blacklight-subject_medical_show') do
+          expect(page).to have_link 'Cats.', href: search_catalog_path({ 'f[subject_facet][]': 'Cats' })
+          expect(page).to have_link 'Hypothalamus.', href: search_catalog_path({ 'f[subject_facet][]': 'Hypothalamus' })
+        end
+      end
+    end
+
+    context 'when viewing contributors' do
+      include_context 'with electronic database record'
+
+      before { visit(solr_document_path(electronic_db_bib)) }
+
+      it 'links to a creator facet search' do
+        within('.col-md-9.blacklight-creator_contributor_show') do
+          expect(page).to have_link('Geo Abstracts, Ltd.',
+                                    href: search_catalog_path({ 'f[creator_facet][]': 'Geo Abstracts, Ltd' }))
+        end
+      end
+    end
+
+    context 'when viewing a conference' do
+      include_context 'with a conference proceedings record with 1 physical holding'
+
+      before do
+        visit(solr_document_path(conference_bib))
+      end
+
+      it 'links to a creator facet search' do
+        show = 'Food and Agriculture Organization of the United Nations (Conference : , 19th : 1977 : Rome, Italy)'
+        facet = 'Food and Agriculture Organization of the United Nations (Conference : , 19th : Rome, Italy)'
+        within('.col-md-9.blacklight-creator_conference_detail_show') do
+          expect(page).to have_link(show, href: search_catalog_path({ 'f[creator_facet][]': facet }))
+        end
+      end
+    end
+  end
+
+  context 'when linking to a fielded search' do
+    context 'with a standardized title' do
+      include_context 'with electronic journal record with 4 electronic entries'
+
+      before do
+        visit(solr_document_path(electronic_journal_bib))
+      end
+
+      it 'links to a title fielded search' do
+        within('.col-md-9.blacklight-title_standardized_show') do
+          expect(page).to have_link('Nature (Online)', href: search_catalog_path(
+            { clause: { '2': { field: 'title_search', query: '"Nature (Online)"' } } }
+          ))
+        end
+      end
+    end
+
+    context 'with a series title' do
+      let(:show_value) do
+        'Report (Expanded Program of Technical Assistance (Food and Agriculture Organization of the United ' \
+          'Nations)) ; C77.'
+      end
+
+      let(:query_value) do
+        '"Report (Expanded Program of Technical Assistance (Food and Agriculture Organization of the United ' \
+          'Nations))"'
+      end
+
+      include_context 'with a conference proceedings record with 1 physical holding'
+
+      before do
+        visit(solr_document_path(conference_bib))
+      end
+
+      it 'links to a title fielded search' do
+        within('.col-md-9.blacklight-series_show') do
+          expect(page).to have_link(show_value, href: search_catalog_path(
+            { clause: { '2': { field: 'title_search', query: query_value } } }
+          ))
+        end
+      end
+    end
+  end
+end
