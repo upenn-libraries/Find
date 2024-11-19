b686a9708ed19b1d2be55f7c6cc6d3ab331c7baa
Mike Kanning <mkanning@upenn.edu>
2024-11-19 15:27:01 -0500

    does the loading issue obtain in CI? add comments on loading issue in Blacklight initializer

--- rails_app/config/initializers/blacklight.rb
+++ rails_app/config/initializers/blacklight.rb
@@ -2,9 +2,11 @@
 
 ActiveSupport::Reloader.to_prepare do
   # TODO: this is throwing Uncaught exception: uninitialized constant Blacklight::Rendering
-  # Blacklight::Rendering::Pipeline.operations = [Blacklight::Rendering::HelperMethod,
-  #                                               Blacklight::Rendering::LinkToFacet,
-  #                                               Blacklight::Rendering::Microdata,
-  #                                               Find::LinkToProcessor,
-  #                                               Find::JoinProcessor]
+  #       could it be related to the addition of zeitwerk in BL 8.4?
+  #       see: https://github.com/projectblacklight/blacklight/pull/3279/files
+  Blacklight::Rendering::Pipeline.operations = [Blacklight::Rendering::HelperMethod,
+                                                Blacklight::Rendering::LinkToFacet,
+                                                Blacklight::Rendering::Microdata,
+                                                Find::LinkToProcessor,
+                                                Find::JoinProcessor]
 end
