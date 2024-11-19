be8b49c8351a34e80b90151f3468706a8e097a0e
Mike Kanning <mkanning@upenn.edu>
2024-11-19 17:23:18 -0500

    update zeitwerk to get around loading issue. remove TODO.

--- rails_app/config/initializers/blacklight.rb
+++ rails_app/config/initializers/blacklight.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 ActiveSupport::Reloader.to_prepare do
-  # TODO: this is throwing Uncaught exception: uninitialized constant Blacklight::Rendering
-  #       could it be related to the addition of zeitwerk in BL 8.4?
-  #       see: https://github.com/projectblacklight/blacklight/pull/3279/files
   Blacklight::Rendering::Pipeline.operations = [Blacklight::Rendering::HelperMethod,
                                                 Blacklight::Rendering::LinkToFacet,
                                                 Blacklight::Rendering::Microdata,
