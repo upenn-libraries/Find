4efa7fd8045fdad157e2bf4598d085173f56fa10
Mike Kanning <mkanning@upenn.edu>
2024-12-11 16:21:14 -0500

    disable sitemap until we can reindex and have the hashed_id_ssi field populated for all records.

--- rails_app/spec/system/sitemap/index_spec.rb
+++ rails_app/spec/system/sitemap/index_spec.rb
@@ -2,7 +2,7 @@
 
 require 'system_helper'
 
-describe 'Sitemap Index Page' do
+xdescribe 'Sitemap Index Page' do
   include_context 'with print monograph record with 2 physical entries'
 
   before { visit blacklight_dynamic_sitemap.sitemap_index_path }
