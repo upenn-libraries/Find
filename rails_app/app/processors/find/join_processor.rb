14a0fc8e861cbf4a0cfa5f7c2c1f1362e25c7de6
Mike Kanning <mkanning@upenn.edu>
2025-02-03 17:06:54 -0500

    reconfigure JoinProcessor to *NOT* join multivalued fields if the request is a JSON request. Add additional fields only to API response. Specs need some attention.

--- rails_app/app/processors/find/join_processor.rb
+++ rails_app/app/processors/find/join_processor.rb
@@ -7,16 +7,27 @@ module Find
     include ActionView::Helpers::TextHelper
 
     def render
-      return next_step(values) if values.blank?
+      return next_step(values) if values.blank? || json_request?
       return next_step(values.first) if values.length == 1
 
-      # rubocop:disable Rails/OutputSafety
-      list = content_tag :ul do
+      next_step unordered_list_of values
+    end
+
+    private
+
+    # @param [Array<String>] values
+    # @return [ActiveSupport::SafeBuffer]
+    # rubocop:disable Rails/OutputSafety
+    def unordered_list_of(values)
+      content_tag :ul do
         safe_join(values.filter_map { |value| content_tag(:li, value.html_safe) })
       end
-      # rubocop:enable Rails/OutputSafety
+    end
+    # rubocop:enable Rails/OutputSafety
 
-      next_step list
+    # @return [Boolean]
+    def json_request?
+      context.search_state.params[:format] == 'json'
     end
   end
 end
