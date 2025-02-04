2737aafbbaa67934d7e8adb31052f93e6f63c5f7
Mike Kanning <mkanning@upenn.edu>
2025-02-04 13:33:57 -0500

    advance adaptations to JoinProcessor spec

--- rails_app/spec/processors/find/join_processor_spec.rb
+++ rails_app/spec/processors/find/join_processor_spec.rb
@@ -2,8 +2,9 @@
 
 describe Find::JoinProcessor do
   let(:config) { Blacklight::Configuration::Field.new(key: 'field') }
+  let(:context) { {} }
   let(:processor) do
-    described_class.new(values, config, SolrDocument.new, {},
+    described_class.new(values, config, SolrDocument.new, context,
                         { context: 'show' }, [Blacklight::Rendering::Terminator])
   end
 
@@ -20,6 +21,15 @@ describe Find::JoinProcessor do
 
     it 'concatenates values into an unordered list' do
       expect(processor.render).to eq('<ul><li>Chapter 1</li><li>Chapter 2</li></ul>')
+    end
+  end
+
+  context 'with a JSON request' do
+    let(:values) { ['Chapter 1', 'Chapter 2'] }
+    let(:context) { Blacklight::SearchContext.new() } # TODO:
+
+    it 'returns an array of untransformed values' do
+      expect(processor.render).to eq values
     end
   end
 end
