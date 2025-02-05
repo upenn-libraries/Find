e453645fc27baf69f71ad94263fc619b7bfbd26c
Amrey Mathurin <mamrey@upenn.edu>
2025-02-05 16:35:26 -0500

    Add PSE source

---
+++ rails_app/app/services/discover/source/pse.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Discover
+  # Class representing Google PSE as a data source - PSE JSON API
+  class Source
+    class PSE < Source
+      # google programmable search engines
+    end
+  end
+end
