0aa1074e9d30658ec9295655f88023a9b3f0d34b
Amrey Mathurin <mamrey@upenn.edu>
2025-02-05 16:36:47 -0500

    Render json results in search controller

--- rails_app/app/controllers/discover/search_controller.rb
+++ rails_app/app/controllers/discover/search_controller.rb
@@ -4,6 +4,30 @@ module Discover
   # Controller for API actions that search over the data sources included in the Discover bento
   class SearchController < ApplicationController
     # /discover/:source/results
-    def results; end
+    # render json from Results object
+    def results
+      source = params[:source]
+      head(:bad_request) unless source.to_sym.in?(Discover::Configuration::SOURCES)
+
+      results = results_for(source: source)
+      render json: results
+    end
+
+    private
+
+    def search_params
+      params.permit :q
+    end
+
+    def results_for(source:)
+      source_klass.new(source: source).results(query: search_params[:q])
+    end
+
+    def source_klass
+      source = params[:source].to_sym
+      return Discover::Source::Blacklight if source.in?(Discover::Configuration::Blacklight::SOURCES)
+
+      Discover::Source::PSE if source.in?(Discover::Configuration::PSE::SOURCES)
+    end
   end
 end
