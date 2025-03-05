dc13c560ff3693f4cfdeb68c955d5832602a83b0
Patrick Perkins <pperkins@upenn.edu>
2025-03-05 12:36:40 -0500

    remove reference to mocks, move fixtures to memoized helpers

--- rails_app/spec/services/discover/parser/art_collection_spec.rb
+++ rails_app/spec/services/discover/parser/art_collection_spec.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
 describe Discover::Parser::ArtCollection do
-  include Discover::ApiMocks::Parser
   include FixtureHelpers
+
+  let(:tsv) { tsv_fixture('art_collection') }
+  let(:tsv_updated) { tsv_fixture('art_collection_updated') }
 
   context 'with new artworks' do
     it 'creates artworks' do
-      expect { described_class.import(file: tsv_fixture('art_collection')) }
+      expect { described_class.import(file: tsv) }
         .to change(Discover::ArtWork, :count).by(9)
     end
   end
 
   context 'with updated artworks' do
-    before { described_class.import(file: tsv_fixture('art_collection')) }
+    before { described_class.import(file: tsv) }
 
     it 'updates changed artworks' do
       format = Discover::ArtWork.first.format
-      described_class.import(file: tsv_fixture('art_collection_updated'))
+      described_class.import(file: tsv_updated)
 
       expect(Discover::ArtWork.first.format).not_to eq format
     end
@@ -24,7 +26,7 @@ describe Discover::Parser::ArtCollection do
     it 'does not update unchanged artworks' do
       format = Discover::ArtWork.second.format
       updated_at = Discover::ArtWork.second.updated_at
-      described_class.import(file: tsv_fixture('art_collection_updated'))
+      described_class.import(file: tsv_updated)
 
       expect(Discover::ArtWork.second.format).to eq format
       expect(Discover::ArtWork.second.updated_at).to eq updated_at
