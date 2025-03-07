cee478c882a46174ea0be2da57f01e2f89493ccf
Patrick Perkins <pperkins@upenn.edu>
2025-03-07 13:54:01 -0500

    update fixtures with new data, check for assigned attributes in spec

--- rails_app/spec/services/discover/parser/art_collection_spec.rb
+++ rails_app/spec/services/discover/parser/art_collection_spec.rb
@@ -7,14 +7,48 @@ describe Discover::Parser::ArtCollection do
   let(:tsv_updated) { tsv_fixture('art_collection_updated') }
 
   context 'with new artworks' do
+    let(:first_artwork) { Discover::ArtWork.first }
+
     before { described_class.import(file: tsv) }
 
     it 'creates artworks' do
-      expect(Discover::ArtWork.count).to eq 9
+      expect(Discover::ArtWork.count).to eq 10
+    end
+
+    it 'assigns title' do
+      expect(first_artwork.title).not_to be_nil
+    end
+
+    it 'assigns link' do
+      expect(first_artwork.link).not_to be_nil
+    end
+
+    it 'assigns identifier' do
+      expect(first_artwork.identifier).not_to be_nil
+    end
+
+    it 'assigns thumbnail_url' do
+      expect(first_artwork.thumbnail_url).not_to be_nil
+    end
+
+    it 'assigns location' do
+      expect(first_artwork.location).not_to be_nil
+    end
+
+    it 'assigns format' do
+      expect(first_artwork.format).not_to be_nil
+    end
+
+    it 'assigns creator' do
+      expect(first_artwork.creator).not_to be_nil
+    end
+
+    it 'assigns description' do
+      expect(first_artwork.description).not_to be_nil
     end
 
     it 'strips html tags in description' do
-      expect(Discover::ArtWork.first.description).not_to match(/<[^>]*>/)
+      expect(first_artwork.description).not_to match(/<[^>]*>/)
     end
   end
 
