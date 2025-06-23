79e982989884bea35cefabaf93c9fbd61eed032e
Mike Kanning <mkanning@upenn.edu>
2025-06-23 17:16:15 -0400

    Don't return specific item renewal information when users are mass-renewing their loans. This was causing a LOT of issues when users used "Renew All" at the end of the semester to renew all of their loans.

--- rails_app/app/controllers/account/requests_controller.rb
+++ rails_app/app/controllers/account/requests_controller.rb
@@ -66,8 +66,15 @@ module Account
     def renew_all
       responses = shelf_service.renew_all_loans
 
-      alert_text = t('account.shelf.renew_all.alerts',
-                     alerts: responses.map { |r| t('account.shelf.renew_all.alert', alert: r.message) }.join)
+      # @note when a very large amount (~100) of loans are renewed, this can lead to a cookie overflow. When there are
+      #       many items being renewed, show a special message without specific item renewal details. This can be
+      #       removed if/when we change our session storage backend.
+      alert_text = if responses.length < 50
+                     t('account.shelf.renew_all.alerts',
+                       alerts: responses.map { |r| t('account.shelf.renew_all.alert', alert: r.message) }.join)
+                   else
+                     t('account.shelf.renew_all.mass_renewal')
+                   end
 
       flash_type = responses.all?(&:renewed?) ? :notice : :alert
       flash[flash_type] = alert_text
