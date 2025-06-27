81a56144cdbde219e73f7e60532c666cdddb955e
Mike Kanning <mkanning@upenn.edu>
2025-06-27 16:26:35 -0400

    yes officer

--- rails_app/app/controllers/catalog_controller.rb
+++ rails_app/app/controllers/catalog_controller.rb
@@ -124,8 +124,8 @@ class CatalogController < ApplicationController
     }
 
     config.add_facet_field :db_subject_facet, label: I18n.t('facets.databases.category'),
-                           show: database_selected,
-                           limit: -1, sort: 'index' do |field|
+                                              show: database_selected,
+                                              limit: -1, sort: 'index' do |field|
       field.include_in_advanced_search = false
     end
     config.add_facet_field :db_sub_subject_facet, label: I18n.t('facets.databases.subject'),
