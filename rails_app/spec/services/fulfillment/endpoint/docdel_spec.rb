1c3e0a8789008546b2d571a02c243ac00e1a59dc
Mike Kanning <mkanning@upenn.edu>
2025-08-11 14:03:00 -0400

    add spec for failure case of docdel fulfillment service

--- rails_app/spec/services/fulfillment/endpoint/docdel_spec.rb
+++ rails_app/spec/services/fulfillment/endpoint/docdel_spec.rb
@@ -40,33 +40,52 @@ describe Fulfillment::Endpoint::Docdel do
   describe '.submit' do
     let(:alma_user_group) { Settings.fulfillment.docdel_user_groups.sample }
     let(:user) { request.requester }
-    let(:message_double) { instance_double ActionMailer::MessageDelivery, deliver_now: true }
 
     include_context 'with mock alma_record on user having alma_user_group user group'
 
-    before do
-      allow(DocdelMailer).to receive(:docdel_email).with(request: request)
-                                                   .and_return(message_double)
-    end
+    context 'with a successful outcome' do
+      let(:message_double) { instance_double ActionMailer::MessageDelivery, deliver_now: true }
 
-    context 'with a successful item-level request' do
-      let(:request) { build(:fulfillment_request, :with_item, :docdel) }
-
-      it 'returns a successful outcome and sends an email' do
-        result = described_class.submit(request: request)
-        expect(result.success?).to be true
-        expect(message_double).to have_received(:deliver_now)
-      end
-    end
-
-    context 'with a successful request form submission' do
-      let(:request) { build(:fulfillment_request, :docdel, :with_section) }
-
-      it 'returns successful outcome' do
-        outcome = described_class.submit(request: request)
-        expect(outcome).to be_success
+      before do
         allow(DocdelMailer).to receive(:docdel_email).with(request: request)
                                                      .and_return(message_double)
+      end
+
+      context 'with a successful item-level request' do
+        let(:request) { build(:fulfillment_request, :with_item, :docdel) }
+
+        it 'sends an email and returns a successful outcome' do
+          result = described_class.submit(request: request)
+          expect(result.success?).to be true
+          expect(message_double).to have_received(:deliver_now)
+        end
+      end
+
+      context 'with a successful request form submission' do
+        let(:request) { build(:fulfillment_request, :docdel, :with_section) }
+
+        it 'sends an email and returns a successful outcome' do
+          outcome = described_class.submit(request: request)
+          expect(outcome).to be_success
+          allow(DocdelMailer).to receive(:docdel_email).with(request: request)
+                                                       .and_return(message_double)
+        end
+      end
+
+      context 'with a failed outcome' do
+        let(:request) { build(:fulfillment_request, :docdel, :with_section) }
+        let(:message) { 'Exception message' }
+
+        before do # simulate a failure in templating
+          allow(DocdelMailer).to receive(:docdel_email).with(request: request)
+                                                       .and_raise(NoMethodError, message)
+        end
+
+        it 'returns a failed outcome' do
+          outcome = described_class.submit(request: request)
+          expect(outcome).to be_failed
+          expect(outcome.errors).to include message
+        end
       end
     end
   end
