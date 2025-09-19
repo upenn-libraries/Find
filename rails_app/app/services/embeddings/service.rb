742f2247da9e60007364037d021217275c5f6f2e
Amrey Mathurin <mamrey@upenn.edu>
2025-09-19 17:21:39 -0400

    Create Embeddings service api

---
+++ rails_app/app/services/embeddings/service.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Embeddings
+  # Orchestrate retrieving embeddings from a LLM
+  class Service
+    attr_reader :input, :client, :parser
+
+    def initialize(input:, client: OllamaClient.new)
+      @client = client
+      @input = input
+    end
+
+    def embeddings
+      @embeddings ||= Embeddings.all(input: input, embeddings: client.embeddings_for(input: input))
+    end
+
+    def commit(record: Subject)
+      embeddings.each do |embedding|
+        record.find_or_create_by(content: embedding.content).update(embedding: embedding.vector)
+      end
+    end
+  end
+end
