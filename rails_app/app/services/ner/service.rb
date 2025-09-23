43af354410ea9eb8d9aafe356ea417d6d1766c1d
Patrick Perkins <pperkins@upenn.edu>
2025-09-23 16:18:12 -0400

    clean up ner service

--- rails_app/app/services/ner/service.rb
+++ rails_app/app/services/ner/service.rb
@@ -6,12 +6,11 @@ module NER
   class Service
     class << self
       def extract_persons(query)
-        return [] if query.empty?
+        return [] if query.blank?
 
         begin
           uri = URI('http://localhost:5001/ner')
           response = Faraday.post(uri, { text: query }.to_json, 'Content-Type' => 'application/json')
-          puts response.body
           person_entities = JSON.parse(response.body).filter_map { |e| e if e['label'] == 'PERSON' }
           person_entities.filter_map { |p| p['text'] }
         rescue StandardError => e
