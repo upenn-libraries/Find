1aaf4d9f15e0d4a906129e6cf12d56419548ff01
Amrey Mathurin <mamrey@upenn.edu>
2025-10-29 15:16:17 -0400

    Implement OnlineAccess engine

---
+++ rails_app/app/services/suggester/engines/online_access.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Suggester
+  module Engines
+    # Suggests a search for the query within 'Online Access'
+    class OnlineAccess < SuggestionEngine
+      EngineRegistry.register(self)
+
+      # @return [Integer]
+      def self.weight
+        2
+      end
+
+      # @return [Suggester::Suggestion]
+      def actions
+        Suggestion.new(entries: [{ label: label,
+                                   url: URI::HTTPS.build(host: Settings.suggestions.digital_catalog.host,
+                                                         query: {
+                                                           "f[access_facet]": 'Online', q: query
+                                                         }.to_query).to_s }],
+                       engine_weight: self.class.weight)
+      end
+
+      private
+
+      # @return [String]
+      def label
+        I18n.t('suggestions.engines.online_access_search.label', query: query)
+      end
+    end
+  end
+end
---
+++ rails_app/spec/services/suggester/engines/online_access_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+describe Suggester::Engines::OnlineAccess do
+  let(:engine) { described_class.new(query: 'test', context: {}) }
+
+  describe '.weight' do
+    it 'returns expected base weight' do
+      expect(described_class.weight).to eq 2
+    end
+  end
+
+  describe '.suggest?' do
+    context 'with a query' do
+      it 'returns true' do
+        expect(described_class.suggest?('test')).to be true
+      end
+    end
+
+    context 'without a query' do
+      it 'returns false' do
+        expect(described_class.suggest?('')).to be false
+      end
+    end
+  end
+
+  describe '#actions' do
+    let(:actions) { engine.actions }
+
+    it 'returns a Suggestions instance' do
+      expect(actions).to be_a(Suggester::Suggestion)
+    end
+
+    it 'returns contains expected entries' do
+      expect(actions).to have_attributes(
+        entries: [{ label: "Search 'test' in online resources.",
+                    url: 'https://find.library.upenn.edu?f%5Baccess_facet%5D=Online&q=test' }]
+      )
+    end
+  end
+
+  describe '#completions' do
+    let(:completions) { engine.completions }
+
+    it 'returns a Suggestions instance' do
+      expect(completions).to be_a(Suggester::Suggestion)
+    end
+
+    it 'contains no entries' do
+      expect(completions).to have_attributes(entries: [])
+    end
+  end
+end
