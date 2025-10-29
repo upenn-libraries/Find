9f4682456a8e47c8103009c444c4700545974664
Amrey Mathurin <mamrey@upenn.edu>
2025-10-29 15:14:26 -0400

    Add spec helpers for creating engine classes

---
+++ rails_app/spec/support/suggester/spec_helpers.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Suggester
+  module SpecHelpers
+    # @return [Class]
+    def mock_engine_class(actions: [], completions: [], query: '', context: {}, success: false)
+      Class.new(Suggester::SuggestionEngine) do
+        @query = query
+        @context = context
+        define_method(:actions) { actions }
+        define_method(:completions) { completions }
+        define_method(:success?) { success }
+      end
+    end
+
+    # @return [Class]
+    def mock_engine_with_actions
+      mock_engine_class(actions: Suggester::Suggestion.new(entries: [{
+                                                             label: 'Search titles for "query"',
+                                                             url: 'https://find.library.upenn.edu/?field=title&q=query'
+                                                           }]), query: 'query', success: true)
+    end
+
+    # @return [Class]
+    def mock_engine_with_completions
+      mock_engine_class(completions: Suggester::Suggestion.new(entries: ['query syntax', 'query language',
+                                                                         'query errors', 'adversarial queries']),
+                        query: 'query', success: true)
+    end
+  end
+end
