723a9e1f48b345ba54276617e0232b2af3dfa032
Amrey Mathurin <mamrey@upenn.edu>
2025-10-29 15:15:33 -0400

    Implement TitleSearch engine

---
+++ rails_app/spec/services/suggester/engines/title_search_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+describe Suggester::Engines::TitleSearch do
+  let(:engine) { described_class.new(query: 'test', context: {}) }
+
+  describe '.weight' do
+    it 'returns expected base weight' do
+      expect(described_class.weight).to eq 5
+    end
+  end
+
+  describe '.suggest?' do
+    context 'with a query' do
+      it 'returns true' do
+        expect(described_class.suggest?('test')).to be true
+      end
+    end
+
+    context 'without a query' do
+      it 'returns false' do
+        expect(described_class.suggest?('')).to be false
+      end
+    end
+  end
+
+  describe '#actions' do
+    let(:actions) { engine.actions }
+
+    it 'returns a Suggestions instance' do
+      expect(actions).to be_a(Suggester::Suggestion)
+    end
+
+    it 'contains expected entries' do
+      expect(actions).to have_attributes(
+        entries: [{ label: "Search 'test' in titles.",
+                    url: 'https://find.library.upenn.edu?q=test&search_field=title_search' }]
+      )
+    end
+  end
+
+  describe '#completions' do
+    let(:completions) { engine.completions }
+
+    it 'returns a Suggestions instance' do
+      expect(completions).to be_a(Suggester::Suggestion)
+    end
+
+    it 'contains no entries' do
+      expect(completions).to have_attributes(entries: [])
+    end
+  end
+end
