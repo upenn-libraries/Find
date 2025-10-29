261021b152c7bf81fff56e1f366f2be8f6c62526
Amrey Mathurin <mamrey@upenn.edu>
2025-10-29 15:11:47 -0400

    Implement SuggestionEngine base class

---
+++ rails_app/spec/services/suggester/suggestion_engine_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+describe Suggester::SuggestionEngine do
+  let(:engine) { described_class.new(query: 'test', context: {}) }
+
+  describe '.weight' do
+    it 'declares the expected weight' do
+      expect(described_class.weight).to eq 0
+    end
+  end
+
+  describe '.suggest?' do
+    it 'returns true when a query is present' do
+      expect(described_class.suggest?('query')).to be true
+    end
+
+    it 'returns false when no query is present' do
+      expect(described_class.suggest?('')).to be false
+    end
+  end
+
+  describe '#actions' do
+    let(:actions) { engine.actions }
+
+    it 'returns a Suggestions instance' do
+      expect(actions).to be_a(Suggester::Suggestion)
+    end
+
+    it 'contains no entries' do
+      expect(actions).to have_attributes(entries: [])
+    end
+  end
+
+  describe '#completions' do
+    let(:completions) { engine.completions }
+
+    it 'returns a Suggestions instance' do
+      expect(completions).to be_a(Suggester::Suggestion)
+    end
+
+    it 'contains no entries' do
+      expect(completions).to have_attributes(entries: [])
+    end
+  end
+
+  describe '#success?' do
+    it 'returns false' do
+      expect(engine.success?).to be false
+    end
+  end
+end
