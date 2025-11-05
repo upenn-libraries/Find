e5dd5cd40d202042d0901c674f2c7eb275901ed5
Amrey Mathurin <mamrey@upenn.edu>
2025-11-05 18:30:26 -0500

    Separate out client class

---
+++ rails_app/app/services/suggester/suggestions/solr/client.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Suggester
+  module Suggestions
+    module Solr
+      # Establishes connection to solr server
+      class Client
+        attr_reader :uri, :params
+
+        def initialize(url:, params:)
+          @uri = URI.parse(url)
+          @params = params
+        end
+
+        def response_body
+          @response_body ||= response.body
+        end
+
+        private
+
+        def response
+          @response ||= connection.get(uri.path, params)
+          byebug
+        rescue Faraday::Error => e
+          Honeybadger.notify(e)
+          raise Service::Error, "Failed to retrieve solr suggestions: #{e}"
+        end
+
+        def connection
+          @connection ||= Faraday.new(uri.to_s) do |conn|
+            conn.request :authorization, :basic, uri.user, uri.password
+            conn.response :json
+            conn.response :raise_error # raise Faraday::Error on status code 4xx or 5xx
+          end
+        end
+      end
+    end
+  end
+end
