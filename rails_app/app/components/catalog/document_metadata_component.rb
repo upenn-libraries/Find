56cfb6f32d02547f087385bc0ed7eb7fd154b737
Patrick Perkins <pperkins@upenn.edu>
2026-01-06 15:43:01 -0500

    fix override comments to point to bl9 instead of beta

--- rails_app/app/components/catalog/document_metadata_component.rb
+++ rails_app/app/components/catalog/document_metadata_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Catalog
-  # Override DocumentMetadataComponent from Blacklight 9.0.0.beta8 to render our custom MetadataFieldLayoutComponent.
+  # Override DocumentMetadataComponent from Blacklight 9.0.0 to render our custom MetadataFieldLayoutComponent.
   class DocumentMetadataComponent < Blacklight::DocumentMetadataComponent
     def initialize(fields: [], **)
       super
--- rails_app/app/components/catalog/metadata_field_layout_component.rb
+++ rails_app/app/components/catalog/metadata_field_layout_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Catalog
-  # Overriding component from Blacklight v9.0.0.beta8 to remove styling from dd tags.
+  # Overriding component from Blacklight v9.0.0 to remove styling from dd tags.
   class MetadataFieldLayoutComponent < Blacklight::MetadataFieldLayoutComponent
     def initialize(field:, value_tag: 'dd', label_class: nil, value_class: nil, offset_class: nil)
       super
