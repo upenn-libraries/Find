7822e99025b7a4d8fe010383f61e9334cfe5de1d
Mike Kanning <mkanning@upenn.edu>
2026-01-12 15:24:02 -0500

    use propshaft rather than sprockets; load bootstrap and popper.js from CDN; remove scss support

--- rails_app/app/javascript/application.js
+++ rails_app/app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
-import "@rails/request.js"
+import "@rails/request.js";
 import "controllers";
-import "popper";
-import "bootstrap";
+import "@popperjs/core";
+import * as bootstrap from "bootstrap";
 import "tom-select";
 import Blacklight from "blacklight-frontend";
--- rails_app/config/importmap.rb
+++ rails_app/config/importmap.rb
@@ -7,8 +7,9 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'popper', to: 'popper.js', preload: true
-pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+# TODO: load BS and popper.js from CDN for simplicity
+pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js', preload: true
+pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.8/dist/js/bootstrap.esm.js', preload: true
 pin 'tom-select', preload: true # @2.3.1
 
 # TODO: fix getAssetPath issue with components
