7d81a214b9b1c6a31cf5f913afaf9291fc9f00da
Mike Kanning <mkanning@upenn.edu>
2026-01-12 15:56:13 -0500

    cleanup importmap config

--- rails_app/config/importmap.rb
+++ rails_app/config/importmap.rb
@@ -7,9 +7,8 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-# TODO: load BS and popper.js from CDN for simplicity
-pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js', preload: true
-pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.8/dist/js/bootstrap.esm.js', preload: true
+pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.8/dist/umd/popper.min.js'
+pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.8/dist/js/bootstrap.js'
 pin 'tom-select', preload: true # @2.3.1
 
 # TODO: fix getAssetPath issue with components
