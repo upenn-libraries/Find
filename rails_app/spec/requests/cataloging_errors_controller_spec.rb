a9efd7cc722f71b441d5acc9ffe4d2c4c57db86d
Patrick Perkins <pperkins@upenn.edu>
2026-01-22 13:00:24 -0500

    test for cataloging errors controller and staff view spec

--- rails_app/spec/requests/cataloging_errors_controller_spec.rb
+++ rails_app/spec/requests/cataloging_errors_controller_spec.rb
@@ -3,51 +3,40 @@
 describe 'cataloging errors requests', type: :request do
   let(:user) { create(:user) }
 
-  describe 'GET /cataloging_errors/new' do
-    context 'when not signed in' do
-      it 'redirects to sign-in' do
-        get cataloging_errors_new_path(mms_id: '123456')
-
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to(new_user_session_path)
+  describe 'POST /cataloging_errors' do
+    context 'when not logged it' do
+      it 'does not send a cataloging error report email' do
+        expect {
+          post cataloging_errors_create_path
+        }.not_to(change { ActionMailer::Base.deliveries.count })
       end
     end
 
-    context 'when signed in' do
+    context 'when logged in' do
       before { login_as user }
 
-      it 'returns a successful response' do
-        get cataloging_errors_new_path(mms_id: '123456')
-
-        expect(response).to have_http_status(:ok)
+      let(:params) do
+        {
+          user: user.id,
+          mms_id: '123456',
+          message: 'The publication date is incorrect.'
+        }
       end
-    end
-  end
 
-  describe 'POST /cataloging_errors' do
-    before { login_as user }
+      it 'sends a cataloging error report email' do
+        expect {
+          post cataloging_errors_create_path, params: params
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
 
-    let(:params) do
-      {
-        user: user.id,
-        mms_id: '123456',
-        message: 'The publication date is incorrect.'
-      }
-    end
-
-    it 'sends a cataloging error report email' do
-      expect {
+      it 'redirects back with a success message' do
         post cataloging_errors_create_path, params: params
-      }.to change { ActionMailer::Base.deliveries.count }.by(1)
-    end
 
-    it 'redirects back with a success message' do
-      post cataloging_errors_create_path, params: params
-
-      expect(response).to have_http_status(:found)
-      expect(flash[:notice]).to eq(
-        I18n.t('cataloging_errors.success')
-      )
+        expect(response).to have_http_status(:found)
+        expect(flash[:notice]).to eq(
+          I18n.t('cataloging_errors.flash.success')
+        )
+      end
     end
   end
 end
--- rails_app/spec/system/catalog/staff_view_spec.rb
+++ rails_app/spec/system/catalog/staff_view_spec.rb
@@ -5,14 +5,42 @@ require 'system_helper'
 describe 'Staff View Page' do
   include_context 'with print monograph record with 2 physical entries'
 
-  before { visit staff_view_solr_document_path(print_monograph_bib) }
+  shared_examples 'staff view page basics' do
+    it 'renders title' do
+      expect(page).to have_content I18n.t('staff_view.title')
+    end
 
-  it 'renders title' do
-    expect(page).to have_content I18n.t('staff_view.title')
+    it 'renders expected fields' do
+      expect(page).to have_selector '.field',
+                                    text: /^#{I18n.t('staff_view.leader', leader: '')}/
+      expect(page).to have_selector '.field', text: /^001/
+    end
   end
 
-  it 'renders expected fields' do
-    expect(page).to have_selector '.field', text: /^#{I18n.t('staff_view.leader', leader: '')}/
-    expect(page).to have_selector '.field', text: /^001/
+  context 'without a logged in user' do
+    before do
+      visit staff_view_solr_document_path(print_monograph_bib)
+    end
+
+    include_examples 'staff view page basics'
+
+    it 'does not show catalog error report button' do
+      expect(page).not_to have_button(I18n.t('cataloging_errors.form.title'))
+    end
+  end
+
+  context 'with a logged in user' do
+    let(:user) { create(:user) }
+
+    before do
+      login_as user
+      visit staff_view_solr_document_path(print_monograph_bib)
+    end
+
+    include_examples 'staff view page basics'
+
+    it 'shows catalog error report button' do
+      expect(page).to have_button(I18n.t('cataloging_errors.form.title'))
+    end
   end
 end
