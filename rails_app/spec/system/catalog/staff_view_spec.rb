51a39a9f585b09400e48db5da09727bbd63e6b1e
Patrick Perkins <pperkins@upenn.edu>
2026-01-28 13:07:16 -0500

    restore staff view spec

--- rails_app/spec/system/catalog/staff_view_spec.rb
+++ rails_app/spec/system/catalog/staff_view_spec.rb
@@ -5,42 +5,14 @@ require 'system_helper'
 describe 'Staff View Page' do
   include_context 'with print monograph record with 2 physical entries'
 
-  shared_examples 'staff view page basics' do
-    it 'renders title' do
-      expect(page).to have_content I18n.t('staff_view.title')
-    end
+  before { visit staff_view_solr_document_path(print_monograph_bib) }
 
-    it 'renders expected fields' do
-      expect(page).to have_selector '.field',
-                                    text: /^#{I18n.t('staff_view.leader', leader: '')}/
-      expect(page).to have_selector '.field', text: /^001/
-    end
+  it 'renders title' do
+    expect(page).to have_content I18n.t('staff_view.title')
   end
 
-  context 'without a logged in user' do
-    before do
-      visit staff_view_solr_document_path(print_monograph_bib)
-    end
-
-    include_examples 'staff view page basics'
-
-    it 'does not show catalog error report button' do
-      expect(page).not_to have_button(I18n.t('cataloging_errors.form.title'))
-    end
-  end
-
-  context 'with a logged in user' do
-    let(:user) { create(:user) }
-
-    before do
-      login_as user
-      visit staff_view_solr_document_path(print_monograph_bib)
-    end
-
-    include_examples 'staff view page basics'
-
-    it 'shows catalog error report button' do
-      expect(page).to have_button(I18n.t('cataloging_errors.form.title'))
-    end
+  it 'renders expected fields' do
+    expect(page).to have_selector '.field', text: /^#{I18n.t('staff_view.leader', leader: '')}/
+    expect(page).to have_selector '.field', text: /^001/
   end
 end
