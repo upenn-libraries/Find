30b1e97f65cf028c8e770e62e5559d2eae940740
Mike Kanning <mkanning@upenn.edu>
2026-02-04 13:16:42 -0500

    use only valid env vars with browserless; adapt configuration for clear usage with cuprite/ferrum; only mount screenshot location in chrome container; block frivilous requests when running system specs

--- rails_app/spec/system/support/cuprite_setup.rb
+++ rails_app/spec/system/support/cuprite_setup.rb
@@ -1,34 +1,24 @@
 # frozen_string_literal: true
 
-# Cuprite config based on https://evilmartians.com/chronicles/system-of-a-test-setting-up-end-to-end-rails-testing
+# Cuprite config inspired by https://evilmartians.com/chronicles/system-of-a-test-setting-up-end-to-end-rails-testing
 
 # First, load Cuprite Capybara integration
 require 'capybara/cuprite'
 
-# Parse URL
-# NOTE: REMOTE_CHROME_HOST should be added to Webmock/VCR allowlist if you use any of those.
-REMOTE_CHROME_URL = ENV.fetch('CHROME_URL', 'ws://localhost:3333')
-
-# Then, we need to register our driver to be able to use it later
-# with #driven_by method.
-# NOTE: The name :cuprite is already registered by Rails.
-# See https://github.com/rubycdp/cuprite/issues/180
+# Register our driver to be able to use it later with #driven_by method.
+# NOTE: The name :cuprite is already registered by Rails. See https://github.com/rubycdp/cuprite/issues/180
 Capybara.register_driver(:better_cuprite) do |app|
   Capybara::Cuprite::Driver.new(
     app,
     **{
-      window_size: [1600, 1200],
-      browser_options: { 'no-sandbox' => nil },
-      # Re-raise js errors in Ruby
-      js_errors: true,
-      # How long to wait for a response from browser before raising a pending connection error
-      timeout: 30,
-      # Increase Chrome startup wait time (required for stable CI builds)
-      process_timeout: 10,
-      # Enable debugging capabilities
-      inspector: true,
+      # See ferrum docs for config options: https://github.com/rubycdp/ferrum
       flatten: false,
-      ws_url: REMOTE_CHROME_URL
+      inspector: !ENV['CI'], # Enable debugging capabilities
+      js_errors: true, # Re-raise js errors in Ruby
+      timeout: 30, # How long to wait for a response from browser before raising a pending connection error
+      url_blacklist: [/typekit/, /favicon/, /footer-bg/], # block some frivolous requests
+      window_size: [1600, 1200],
+      ws_url: ENV.fetch('CHROME_URL', 'ws://localhost:3333')
     }
   )
 end
@@ -38,12 +28,13 @@ Capybara.default_driver = Capybara.javascript_driver = :better_cuprite
 
 module CupriteHelpers
   # Drop #pause anywhere in a test to stop the execution.
-  # Useful when you want to checkout the contents of a web page in the middle of a test
+  # Useful when you want to check out the contents of a web page in the middle of a test
   # running in a headful mode.
   def pause
     page.driver.pause
   end
 
+  # TODO: this isn't working now that browserless2 runs over websockets
   def debug(binding = nil)
     $stdout.puts '🔎 Open Chrome inspector at http://localhost:3333'
     return binding.break if binding
