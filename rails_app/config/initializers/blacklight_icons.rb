d22fb3ff8ebea3156dff74fbd7563fa33dadbb00
Jon Earley <earleyj@upenn.edu>
2026-02-11 09:58:06 -0500

    Trying out hiding "Searching for" since maybe it's a better use of space to remove it.

--- rails_app/config/initializers/blacklight_icons.rb
+++ rails_app/config/initializers/blacklight_icons.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Override Blacklight's default remove (x) icon with a simpler design
-Rails.application.config.after_initialize do
+Rails.application.config.to_prepare do
   Blacklight::Icons::RemoveComponent.svg = <<~SVG
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none">
       <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
