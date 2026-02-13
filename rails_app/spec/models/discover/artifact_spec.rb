55e8288f5499e13c30886a7fb1d7d3f9c28bb43e
Patrick Perkins <pperkins@upenn.edu>
2026-02-13 10:01:43 -0500

    clean up artifact spec

--- rails_app/spec/models/discover/artifact_spec.rb
+++ rails_app/spec/models/discover/artifact_spec.rb
@@ -1,26 +1,31 @@
 # frozen_string_literal: true
 
 describe Discover::Artifact do
-  it 'requires a title' do
-    artwork = build(:artifact, title: nil)
-    expect(artwork.valid?).to be false
-    expect(artwork.errors[:title]).to include("can't be blank")
-  end
+  describe 'validations' do
+    it 'is invalid without a title' do
+      artifact = build(:artifact, title: nil)
 
-  it 'requires a link' do
-    artwork = build(:artifact, link: nil)
-    expect(artwork.valid?).to be false
-    expect(artwork.errors[:link]).to include("can't be blank")
+      expect(artifact).not_to be_valid
+      expect(artifact.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without a link' do
+      artifact = build(:artifact, link: nil)
+
+      expect(artifact).not_to be_valid
+      expect(artifact.errors[:link]).to include("can't be blank")
+    end
   end
 
   describe '.search' do
     let!(:statue) { create(:artifact, location: 'America') }
     let!(:brick) do
-      create(:artifact, title: 'Brick',
-                        format: 'Clay',
-                        description: 'This is an amazing clay brick',
-                        location: 'Near Eastern',
-                        on_display: true)
+      create(:artifact,
+             title: 'Brick',
+             format: 'Clay',
+             description: 'This is an amazing clay brick',
+             location: 'Near Eastern',
+             on_display: true)
     end
     let!(:stela) do
       create(:artifact,
@@ -31,52 +36,36 @@ describe Discover::Artifact do
              on_display: false)
     end
 
-    context 'when the query fully matches a single artifact' do
-      it 'returns the expected artifact' do
-        expect(described_class.search('statue fragment')).to contain_exactly(statue)
-      end
+    it 'returns artifact matching full query' do
+      expect(described_class.search('statue fragment')).to contain_exactly(statue)
     end
 
-    context 'when the query partially matches a single artifact' do
-      it 'returns the expected artifact' do
-        expect(described_class.search('clay')).to contain_exactly(brick)
-      end
+    it 'returns artifact matching partial query' do
+      expect(described_class.search('clay')).to contain_exactly(brick)
     end
 
-    context 'when the query fully matches multiple artifacts' do
-      it 'returns the expected artifacts' do
-        expect(described_class.search('near eastern')).to contain_exactly(brick, stela)
-      end
+    it 'returns multiple artifacts when query matches both' do
+      expect(described_class.search('near eastern')).to contain_exactly(brick, stela)
     end
 
-    context 'when the query is in a different case' do
-      it 'performs a case insensitive search' do
-        expect(described_class.search('LIMESTONE')).to contain_exactly(stela)
-      end
+    it 'performs case insensitive search' do
+      expect(described_class.search('LIMESTONE')).to contain_exactly(stela)
     end
 
-    context 'when the query matches multiple artifacts with varying degrees of relevancy' do
-      it 'returns artifacts sorted by relevancy' do
-        expect(described_class.search('brick stela')).to eq [brick, stela]
-      end
+    it 'returns artifacts sorted by relevancy' do
+      expect(described_class.search('brick stela')).to eq [brick, stela]
     end
 
-    context 'when the query has no matching artifacts' do
-      it 'returns no artifacts' do
-        expect(described_class.search('Salvador Dali')).to eq []
-      end
+    it 'returns no results for non-matching query' do
+      expect(described_class.search('Salvador Dali')).to be_empty
     end
 
-    context 'when the query is blank' do
-      it 'returns no artifacts' do
-        expect(described_class.search('')).to eq []
-      end
+    it 'returns no results for blank query' do
+      expect(described_class.search('')).to be_empty
     end
 
-    context "when the query is '*'" do
-      it 'does not return all artifacts' do
-        expect(described_class.search('*')).to eq []
-      end
+    it 'returns no results for wildcard query' do
+      expect(described_class.search('*')).to be_empty
     end
   end
 end
