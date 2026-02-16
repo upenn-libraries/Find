34cc8b50fd96fe3946269cb53f9e95b056e8a2c7
Amrey Mathurin <mamrey@upenn.edu>
2026-02-16 12:22:15 -0500

    Create Harvest model

---
+++ rails_app/app/models/discover/harvest.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Discover
+  # Represents a harvest of an external resource
+  class Harvest < ApplicationRecord
+    validates :source, presence: true, uniqueness: true
+
+    self.table_name = 'discover_harvests'
+
+    # @return [Hash]
+    def headers
+      {
+        'If-None-Match': etag,
+        'If-Modified-Since': resource_last_modified_httpdate
+      }
+    end
+
+    # @return [Hash]
+    def update_from_response_headers!(response_headers)
+      update!(resource_last_modified: response_headers['last-modified'], etag: response_headers['etag'])
+    end
+
+    private
+
+    # @return [String]
+    def resource_last_modified_httpdate
+      return unless resource_last_modified
+
+      resource_last_modified.httpdate
+    end
+  end
+end
