168b6cd091942a68f2bff202c1d54eb19ac912c1
Amrey Mathurin <mamrey@upenn.edu>
2026-02-17 13:01:01 -0500

    Extract downloader into its own class

---
+++ rails_app/app/services/discover/harvester/downloader.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Discover
+  module Harvester
+    # Streams external resource to a Tempfile
+    class Downloader
+      # @param connection [Discover::Harvester::Connection]
+      def initialize(connection:)
+        @connection = connection
+      end
+
+      # An array containing the Faraday response object and the downloaded Tempfile or Nil if the response does not have
+      # 2xx status code.
+      # @param [String] path
+      # @param [String] filename
+      # @param [String] extension
+      # @param [Hash] headers
+      # @return [Array(Faraday::Response, Tempfile | nil)]
+      def download(path:, filename:, extension:, headers: {})
+        tempfile = nil
+        response = @connection.get(path, headers: headers) do |req|
+          req.options.on_data = proc do |chunk, _overall_received_bytes, env|
+            # Do not create empty TempFile when resource has not been modified
+            next if env.status == 304
+
+            tempfile ||= Tempfile.new([filename, extension], binmode: true)
+            tempfile.write chunk
+          end
+        end
+        tempfile&.rewind
+        [response, tempfile]
+      rescue StandardError => e
+        tempfile&.close!
+        raise e
+      end
+    end
+  end
+end
--- rails_app/spec/support/discover/api_mocks/harvester/penn_museum.rb
+++ rails_app/spec/support/discover/api_mocks/harvester/penn_museum.rb
@@ -6,12 +6,12 @@ module Discover
       module PennMuseum
         # @param body [String] contents to download
         # @param status [Integer] response http status
-        # @param headers [Hash] response headers
-        def stub_csv_download_response(body:, status:, headers:)
+        # @param response_headers [Hash] response headers
+        def stub_csv_download_response(body:, status:, response_headers:)
           host = Settings.discover.source.penn_museum.host
           path = Settings.discover.source.penn_museum.csv.path
           stub_request(:get, URI::HTTPS.build(host: host, path: path))
-            .to_return_json(status: status, body: body, headers: headers)
+            .to_return_json(status: status, body: body, headers: response_headers)
         end
       end
     end
