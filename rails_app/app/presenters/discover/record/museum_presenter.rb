1f9c62b7f71848843bb5eaecd6522396804d8dfc
Mike Kanning <mkanning@upenn.edu>
2026-02-20 11:31:56 -0500

    render thumbnails from AWS s3 bucket using presigned URLs

--- rails_app/app/presenters/discover/record/museum_presenter.rb
+++ rails_app/app/presenters/discover/record/museum_presenter.rb
@@ -13,6 +13,23 @@ module Discover
       def location
         record.title&.first&.split('|')&.last&.strip
       end
+
+      def thumbnail_url
+        # db_value = record.thumbnail_url
+        db_value = '10002_300.jpg'
+        return nil if db_value.blank?
+
+        s3 = Aws::S3::Resource.new(
+          region: Settings.discover.thumbnails.aws.region,
+          credentials: Aws::Credentials.new(
+            Settings.discover.thumbnails.aws.access_key,
+            Settings.discover.thumbnails.aws.secret_access_key
+          )
+        )
+        obj = s3.bucket(Settings.discover.thumbnails.aws.bucket).object(db_value)
+
+        obj.presigned_url(:get, expires_in: 300)
+      end
     end
   end
 end
