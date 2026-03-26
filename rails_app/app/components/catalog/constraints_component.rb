b01defb130f0a8cd48254f246c04ddd361621f43
Jon Earley <earleyj@upenn.edu>
2026-03-26 09:04:02 -0400

    remove unnecessary redefined methods

--- rails_app/app/components/catalog/constraints_component.rb
+++ rails_app/app/components/catalog/constraints_component.rb
@@ -64,46 +64,6 @@ module Catalog
 
     private
 
-    # Creates a facet constraint presenter for a single facet item
-    #
-    # @param facet_field_presenter [Blacklight::FacetFieldPresenter] presenter for the facet field
-    # @param facet_config [Blacklight::Configuration::FacetField] configuration for the facet
-    # @param facet_item [String] the facet item
-    # @return [Blacklight::ConstraintPresenter] constraint presenter for the facet item
-    def facet_constraint_presenter(facet_field_presenter, facet_config, facet_item)
-      facet_config.constraint_presenter.new(facet_item_presenter: facet_field_presenter.item_presenter(facet_item),
-                                            field_label: facet_field_presenter.label)
-    end
-
-    # Creates a constraint presenter for an inclusive facet (multiple values)
-    #
-    # @param facet_field_presenter [Blacklight::FacetFieldPresenter] presenter for the facet field
-    # @param facet_config [Blacklight::Configuration::FacetField] configuration for the facet
-    # @param facet_item [Array] array of facet items
-    # @param facet_field [Symbol, String] the facet field name
-    # @return [Blacklight::ConstraintPresenter] constraint presenter for the inclusive facet
-    def inclusive_facet_constraint_presenter(facet_field_presenter, facet_config, facet_item, facet_field)
-      item_presenter = Blacklight::InclusiveFacetItemPresenter.new(
-        facet_item, facet_config, helpers, facet_field
-      )
-      facet_config.constraint_presenter.new(
-        facet_item_presenter: item_presenter,
-        field_label: facet_field_presenter.label
-      )
-    end
-
-    # Returns a heading tag for the constraints section
-    #
-    # @return [ActiveSupport::SafeBuffer, nil] constraints heading html
-    def constraints_heading
-      return unless @render_headers
-
-      tag.h2(
-        t('blacklight.search.filters.title'),
-        class: @heading_classes
-      )
-    end
-
     # Checks if advanced search clause queries are blank
     #
     # @return [Boolean] true if no clause queries present
