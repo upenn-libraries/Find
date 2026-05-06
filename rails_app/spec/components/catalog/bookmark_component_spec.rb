9114113497a7a1479add56e31e17ec426885e530
Patrick Perkins <pperkins@upenn.edu>
2026-05-06 14:17:22 -0400

    simplify component spec

--- rails_app/spec/components/catalog/bookmark_component_spec.rb
+++ rails_app/spec/components/catalog/bookmark_component_spec.rb
@@ -6,27 +6,19 @@ describe Catalog::BookmarkComponent, type: :components do
   let(:rendered) { render_inline(component) }
   let(:bookmark_label) { rendered.at_css('.toggle-bookmark-label') }
 
-  shared_examples 'an accessible bookmark tooltip' do |translation_key|
-    it 'renders the bookmark control with an accessible tooltip label' do
-      expected_label = I18n.t(translation_key)
-
-      expect(bookmark_label['data-action']).to eq(described_class::TOOLTIP_REFRESH_ACTION)
-      expect(bookmark_label['data-tooltip-content-selector-value']).to eq(described_class::TOOLTIP_CONTENT_SELECTOR)
-      expect(bookmark_label['data-controller']).to eq('tooltip')
-      expect(bookmark_label['data-bs-title']).to eq(expected_label)
-      expect(bookmark_label['aria-label']).to eq(expected_label)
-    end
-  end
-
   context 'when the document is not bookmarked' do
     let(:checked) { false }
 
-    include_examples 'an accessible bookmark tooltip', 'blacklight.search.bookmarks.absent'
+    it 'renders the bookmark control with absent lable' do
+      expect(bookmark_label['data-bs-title']).to eq(I18n.t('blacklight.search.bookmarks.absent'))
+    end
   end
 
   context 'when the document is bookmarked' do
     let(:checked) { true }
 
-    include_examples 'an accessible bookmark tooltip', 'blacklight.search.bookmarks.present'
+    it 'renders the bookmark control with present lable' do
+      expect(bookmark_label['data-bs-title']).to eq(I18n.t('blacklight.search.bookmarks.present'))
+    end
   end
 end
