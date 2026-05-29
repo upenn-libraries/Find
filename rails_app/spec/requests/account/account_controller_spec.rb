0f69cb81393fb65da7acfbb0d634aed825e370ec
Jon Earley <earleyj@upenn.edu>
2026-05-29 10:18:42 -0400

    test coverage

--- rails_app/spec/requests/account/account_controller_spec.rb
+++ rails_app/spec/requests/account/account_controller_spec.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 describe 'Account Controller Requests' do
-  it_behaves_like 'an authenticated controller' do
-    let(:path) { account_path }
+  context 'when not authenticated' do
+    it 'responds successfully' do
+      get account_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context 'when authenticated' do
+    let(:user) { create(:user) }
+
+    before do
+      login_as user
+      get account_path
+    end
+
+    it 'responds successfully' do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
